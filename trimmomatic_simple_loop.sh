21cc007f6f6b9097d98055573c74383616013eb7
nikostourvas <nikostourvas@gmail.com>
2022-05-30 13:15:18 +0200

    update trimmomatic script, add VarScan script

--- trimmomatic_simple_loop.sh
+++ trimmomatic_simple_loop.sh
@@ -1,9 +1,14 @@
-for infile in *1_001.fastq.gz
+#!/bin/bash
+
+UNTRIMMED=/home/tourvasn/ngs_training/data/untrimmed_fastq/
+TRIMMED=/home/tourvasn/ngs_training/results/trimmed_fastq/
+
+for infile in ${UNTRIMMED}*1_001.fastq.gz
  do
    base=$(basename ${infile} 1_001.fastq.gz)
-   java -jar /programs/Trimmomatic-0.39/trimmomatic-0.39.jar PE ${infile} ${base}2_001.fastq.gz \
-                ${base}_1.trim.fastq.gz ${base}_1un.trim.fastq.gz \
-                ${base}_2.trim.fastq.gz ${base}_2un.trim.fastq.gz \
+   java -jar /programs/Trimmomatic-0.39/trimmomatic-0.39.jar PE ${infile} ${UNTRIMMED}${base}2_001.fastq.gz \
+                ${TRIMMED}${base}_1.trim.fastq.gz ${TRIMMED}${base}_1un.trim.fastq.gz \
+                ${TRIMMED}${base}_2.trim.fastq.gz ${TRIMMED}${base}_2un.trim.fastq.gz \
                 SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 \
 		-threads 10
  done
---
+++ variant_calling_samtools-VarScan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DATA="/home/tourvasn/ngs_training/results/align/Plomion"
+RESULTS="/home/tourvasn/ngs_training/results/VCF"
+REF="/home/tourvasn/ngs_training/data/reference/Plomion_et_al_2018/Qrob_V2_2N.fa"
+	
+/programs/samtools/bin/samtools mpileup -B -q 1 -f $REF $DATA/*_sort.bam > $RESULTS/cohort.mpileup
+java -jar /home/nikos/NGS/VarScan.jar mpileup2snp $RESULTS/cohort.mpileup \
+	--vcf-sample-list inds \
+	--min-coverage 10 --min-var-freq 0.0125 --p-value 0.05 \
+	--output-vcf 1 > $RESULTS/cohort.varScan.snp.vcf
