21cf5593c30e6a9bbfe62f893f69979b7cf2e945
nikostourvas <nikostourvas@gmail.com>
2022-06-01 15:39:50 +0200

    set parameters for bwa and samtools

--- parallel_bwa.sh
+++ parallel_bwa.sh
@@ -6,7 +6,7 @@ BASE="/home/tourvasn/ngs_training"
 mkdir $BASE/results/align
 
 # align a single individual
-REF=$BASE/data/reference/PRJEB51283.fasta
+REF=$BASE/data/reference/Plomion_et_al_2018/Qrob_V2_2N.fa
 
 # declare variables
 IND=$1
@@ -16,10 +16,11 @@ OUTPUT=$BASE/results/align/${IND}_sort.bam
 
 # then align and sort
 # -t how many cores to use PER SAMPLE
+# -@ how many cores to use PER SAMPLE
 echo "Aligning $IND with bwa"
 bwa mem -M -t 15 $REF $FORWARD \
-$REVERSE | samtools view -b | \
-samtools sort -T ${IND} > $OUTPUT
+$REVERSE | samtools view -@ 15 -q 20 -b | \
+samtools sort -@ 15 -T ${IND} > $OUTPUT
 
 # statistics about sorted bam file
 samtools flagstat $OUTPUT > $BASE/results/align/${IND}_sort.bam.flagstats
