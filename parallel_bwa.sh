8dee84a87190c30fe0b22de2d95a9d32c4588bb4
nikostourvas <nikostourvas@gmail.com>
2022-06-02 13:10:51 +0200

    remove duplicates with samtools instead of picard

--- parallel_bwa.sh
+++ parallel_bwa.sh
@@ -6,7 +6,7 @@ BASE="/home/tourvasn/ngs_training"
 mkdir $BASE/results/align
 
 # align a single individual
-REF=$BASE/data/reference/Plomion_et_al_2018/Qrob_V2_2N.fa
+REF=$BASE/data/reference/Qrob_PM1N.fa
 
 # declare variables
 IND=$1
@@ -14,12 +14,12 @@ FORWARD=$BASE/results/trimmed_fastq/${IND}_R_1.trim.fastq.gz
 REVERSE=$BASE/results/trimmed_fastq/${IND}_R_2.trim.fastq.gz
 OUTPUT=$BASE/results/align/${IND}_sort.bam
 
-# then align and sort
+# then align, filter for quality, remove duplicates, and sort
 # -t how many cores to use PER SAMPLE
 # -@ how many cores to use PER SAMPLE
-echo "Aligning $IND with bwa"
-bwa mem -M -t 15 $REF $FORWARD \
-$REVERSE | samtools view -@ 15 -q 20 -b | \
+bwa mem -M -t 15 $REF $FORWARD $REVERSE | \
+samtools view -@ 15 -q 20 -b | \
+samtools rmdup | \
 samtools sort -@ 15 -T ${IND} > $OUTPUT
 
 # statistics about sorted bam file
