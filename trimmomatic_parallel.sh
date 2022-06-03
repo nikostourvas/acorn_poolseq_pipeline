f77a94a57d4822a961ae669c56c005234ad91dab
nikostourvas <nikostourvas@gmail.com>
2022-06-03 11:48:07 +0200

    ind names fix

--- trimmomatic_parallel.sh
+++ trimmomatic_parallel.sh
@@ -9,7 +9,7 @@ UNTRIMMED=/home/tourvasn/ngs_training/data/untrimmed_fastq/
 TRIMMED=/home/tourvasn/ngs_training/results/trimmed_fastq/
 
 java -jar /programs/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
-	${UNTRIMMED}${IND}_1.fastq.gz ${UNTRIMMED}${IND}_2.fastq.gz \
+	${UNTRIMMED}${IND}_R1_001.fastq.gz ${UNTRIMMED}${IND}_R2_001.fastq.gz \
         ${TRIMMED}${IND}_1.trim.fastq.gz ${TRIMMED}${IND}_1un.trim.fastq.gz \
         ${TRIMMED}${IND}_2.trim.fastq.gz ${TRIMMED}${IND}_2un.trim.fastq.gz \
         SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:TruSeq3-PE-2.fa:2:40:15 \
