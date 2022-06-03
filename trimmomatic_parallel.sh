c33abf0388c7c49b3f279905ef2dfe7d7dcb6655
nikostourvas <nikostourvas@gmail.com>
2022-06-03 11:44:00 +0200

    fix path

--- trimmomatic_parallel.sh
+++ trimmomatic_parallel.sh
@@ -8,7 +8,7 @@ IND=$1
 UNTRIMMED=/home/tourvasn/ngs_training/data/untrimmed_fastq/
 TRIMMED=/home/tourvasn/ngs_training/results/trimmed_fastq/
 
-java -jar ~/NGS/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
+java -jar /programs/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
 	${UNTRIMMED}${IND}_1.fastq.gz ${UNTRIMMED}${IND}_2.fastq.gz \
         ${TRIMMED}${IND}_1.trim.fastq.gz ${TRIMMED}${IND}_1un.trim.fastq.gz \
         ${TRIMMED}${IND}_2.trim.fastq.gz ${TRIMMED}${IND}_2un.trim.fastq.gz \
