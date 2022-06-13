7e1b6320514d485b9e0bc2a7b86e506995df46c9
nikostourvas <nikostourvas@gmail.com>
2022-06-13 15:16:45 +0200

    incorporate fixmates

--- bam_cleaning.sh
+++ bam_cleaning.sh
@@ -2,17 +2,21 @@
 
 BASE="/home/tourvasn/ngs_training"
 
-# reference genome
-REF=$BASE/data/reference/Qrob_PM1N.fa
-
 # declare variables
-IND=$1
-FORWARD=$BASE/results/trimmed_fastq/${IND}_R_1.trim.fastq.gz
-REVERSE=$BASE/results/trimmed_fastq/${IND}_R_2.trim.fastq.gz
-OUTPUT=$BASE/results/align/${IND}
+IND=${1}
+OUTPUT=${BASE}/results/align/${IND}
 
-samtools view -@ 15 -bh -q 20 ${OUTPUT}.sort.bam > ${OUTPUT}.sort.Q20.bam
-samtools rmdup ${OUTPUT}.sort.Q20.bam ${OUTPUT}.sort.Q20.nodup.bam
-samtools index -@ 15 ${OUTPUT}.sort.Q20.nodup.bam
-samtools flagstat ${OUTPUT}.sort.Q20.nodup.bam > ${OUTPUT}.sort.Q20.nodup.stats
-rm ${OUTPUT}${IND}.raw.bam ${OUTPUT}${IND}.sort.bam ${OUTPUT}${IND}.sort.Q20.bam
+# Re-sort the reads according to their genomic coordinates
+samtools sort -@ 15 ${OUTPUT}.fixmate.sort.bam > ${OUTPUT}.fixmate.newsort.bam
+# Quality filter 
+samtools view -@ 15 -bh -q 20 ${OUTPUT}.fixmate.newsort.bam > ${OUTPUT}.sort.Q20.bam
+# mark the duplicate reads as variant callers take this information into account
+# here we don't use 'samtools rmdup' as it is no longer recommended to remove duplicates
+# marking the duplicates is enough
+samtools markdup -@ 15 ${OUTPUT}.sort.Q20.bam ${OUTPUT}.sort.Q20.markdup.bam
+# index
+samtools index -@ 15 ${OUTPUT}.sort.Q20.markdup.bam
+# gather stats
+samtools flagstat ${OUTPUT}.sort.Q20.markdup.bam > ${OUTPUT}.sort.Q20.markdup.stats
+# remove unnecessary files
+#rm ${OUTPUT}${IND}.raw.bam ${OUTPUT}${IND}.sort.bam ${OUTPUT}${IND}.sort.Q20.bam
--- mapping.sh
+++ mapping.sh
@@ -3,22 +3,27 @@
 BASE="/home/tourvasn/ngs_training"
 
 # create output directory
-mkdir $BASE/results/align
+mkdir -p ${BASE}/results/align
 
 # reference genome
-REF=$BASE/data/reference/Qrob_PM1N.fa
+REF=${BASE}/data/reference/Qrob_PM1N.fa
 
 # declare variables
-IND=$1
-FORWARD=$BASE/results/trimmed_fastq/${IND}_R_1.trim.fastq.gz
-REVERSE=$BASE/results/trimmed_fastq/${IND}_R_2.trim.fastq.gz
-OUTPUT=$BASE/results/align/${IND}
+IND=${1}
+FORWARD=${BASE}/results/trimmed_fastq/${IND}_1.trim.fastq.gz
+REVERSE=${BASE}/results/trimmed_fastq/${IND}_2.trim.fastq.gz
+RG="@RG\tID:${IND}\tPL:Illumina\tSM:${IND}"
+OUTPUT=${BASE}/results/align/${IND}
 
 # then align, filter for quality, remove duplicates, and sort
 # -t how many cores to use PER SAMPLE
+bwa mem -R ${RG} -M -t 15 ${REF} ${FORWARD} ${REVERSE} > ${OUTPUT}.raw.sam
+
 # -@ how many cores to use PER SAMPLE
-bwa mem -R "@RG\tID:${IND}\tPL:Illumina\tSM:${IND}" \
-	-M -t 15 $REF $FORWARD $REVERSE | \
-samtools view -@ 15 -b > ${OUTPUT}.raw.bam
-samtools sort -@ 15 ${OUTPUT}.raw.bam -o ${OUTPUT}.sort.bam
-samtools flagstat ${OUTPUT}.sort.bam > ${OUTPUT}.sort.stats
+samtools view -@ 15 -b ${OUTPUT}.raw.sam > ${OUTPUT}.raw.bam
+# sort reads in the BAM according to their names so that pairs are placed one below the other
+samtools sort -n -@ 15 ${OUTPUT}.raw.bam > ${OUTPUT}.name.sort.bam
+# fix read-mates so that they both have the same sets of attributesfor the subsequent preprocessing
+samtools fixmate -m ${OUTPUT}.name.sort.bam ${OUTPUT}.fixmate.sort.bam
+# gather statistics
+samtools flagstat ${OUTPUT}.fixmate.sort.bam > ${OUTPUT}.sort.stats
