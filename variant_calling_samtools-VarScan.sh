e69f2e92a14fc176b4cf8677da435f63af8cb082
nikostourvas <nikostourvas@gmail.com>
2022-06-13 15:16:26 +0200

    mkdir error and minor

--- variant_calling_samtools-VarScan.sh
+++ variant_calling_samtools-VarScan.sh
@@ -4,9 +4,9 @@ DATA="/home/tourvasn/ngs_training/results/align"
 RESULTS="/home/tourvasn/ngs_training/results/VCF"
 REF="/home/tourvasn/ngs_training/data/reference/Qrob_PM1N.fa"
 
-mkdir ../results/VCF
+mkdir -p ../results/VCF
 	
-samtools mpileup -B -q 1 -f $REF $DATA/*.sort.Q20.nodup.bam > $RESULTS/cohort.mpileup
+samtools mpileup -B -q 1 -f $REF $DATA/*.sort.Q20.markdup.bam > $RESULTS/cohort.mpileup
 java -jar /programs/VarScan.jar mpileup2snp $RESULTS/cohort.mpileup \
 	--vcf-sample-list inds \
 	--min-coverage 8 --min-var-freq 0.0055 --p-value 0.05 \
