8b1d855794a4071910123e51a0315ee7ae07c7fc
nikostourvas <nikostourvas@gmail.com>
2022-06-16 20:05:19 +0200

    update for parallel snp calling

--- draft_pipeline.sh
+++ draft_pipeline.sh
@@ -20,5 +20,17 @@ parallel --verbose -j 2 \
 parallel --verbose -j 2 \
 	'bash bam_cleaning.sh {}' :::: inds
 
+# make list of all chromosomes & scaffolds
+grep 'Qrob' /home/tourvasn/ngs_training/data/reference/Qrob_PM1N.fa | \
+ cut -c2- > regions 
+
 # Variant calling
 bash variant_calling_samtools-VarScan.sh
+parallel --verbose -j 20 \
+	'bash variant_calling_parallel.sh {}' :::: regions
+
+bash vcf_merge.sh
+
+# filter for paralogs
+# extremely RAM hungry - DO NOT RUN
+#singularity exec ~/singularity_images/bam-readcount.sif false_positive_filter.sh
