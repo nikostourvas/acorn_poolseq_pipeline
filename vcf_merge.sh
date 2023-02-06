7e5d974f4c3cfe01d07e6d59212c26b4aee61ba6
nikostourvas <nikostourvas@gmail.com>
2023-02-06 17:44:46 +0200

    remove step for filtering snps close to indels

--- vcf_merge.sh
+++ vcf_merge.sh
@@ -34,14 +34,3 @@ done
 # Merge VCFs
 bcftools concat --threads 32 $RESULTS/Qrob*.snp.vcf.gz -o $RESULTS/Qrob_total.snp.vcf
 bcftools concat --threads 32 $RESULTS/Qrob*.indel.vcf.gz -o $RESULTS/Qrob_total.indel.vcf
-
-# Filter SNPs close to InDels
-java -jar /usr/share/java/varscan.jar filter $RESULTS/Qrob_total.snp.vcf \
-    --min-var-freq 0.0055 --p-value 0.05 --min-avg-qual 20 \
-    --min-coverage 50 --min-reads2 2 \
-    --indel-file $RESULTS/Qrob_total.indel.vcf \
-    --output-file $RESULTS/Qrob_total_filter.snp.vcf
-
-# keep only biallelic SNPs
-bcftools view --threads 32 -m2 -M2 -v snps $RESULTS/Qrob_total_filter.snp.vcf \
-    -o $RESULTS/Qrob_total_filter2.snp.vcf
