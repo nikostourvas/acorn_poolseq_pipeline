b56e021eec96296477b4c38148341fe119eb2ea6
nikostourvas <nikostourvas@gmail.com>
2023-02-07 11:58:37 +0200

    edit java executable path

--- variant_calling_parallel.sh
+++ variant_calling_parallel.sh
@@ -12,12 +12,12 @@ REGION=$1
 samtools mpileup -B -q 1 -r $REGION -f $REF $DATA/*.sort.Q20.markdup.bam \
     > $RESULTS/$REGION.mpileup
 
-java -jar /programs/VarScan.jar mpileup2snp $RESULTS/$REGION.mpileup \
+java -jar /usr/share/java/varscan.jar mpileup2snp $RESULTS/$REGION.mpileup \
 	--vcf-sample-list inds \
 	--min-coverage 8 --min-var-freq 0.0055 --p-value 0.05 \
 	--output-vcf 1 > $RESULTS/$REGION.varScan.snp.vcf
 
-java -jar /programs/VarScan.jar mpileup2indel $RESULTS/$REGION.mpileup \
+java -jar /usr/share/java/varscan.jar mpileup2indel $RESULTS/$REGION.mpileup \
 	--vcf-sample-list inds \
 	--min-coverage 8 --min-var-freq 0.0055 --p-value 0.1 \
 	--output-vcf 1 > $RESULTS/$REGION.varScan.indel.vcf
