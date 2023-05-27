fc8be40293809ff1161ac9f297dc4916d7ddb3a7
nikostourvas <nikostourvas@gmail.com>
2023-05-27 23:10:45 +0300

    bcftools statistics

--- vcf_biallelic.sh
+++ vcf_biallelic.sh
@@ -2,7 +2,11 @@
 
 # declare variables
 RESULTS=../results/VCF
+REF=../data/reference/Qrob_PM1N.fa
 
 # keep only biallelic SNPs
 bcftools view --threads 32 -m2 -M2 -v snps $RESULTS/Qrob_total_filter.snp.vcf \
     -o $RESULTS/Qrob_total_filter2.snp.vcf
+
+# produce statistics
+bcftools stats --fasta-ref $REF $RESULTS/Qrob_total_filter2.snp.vcf
