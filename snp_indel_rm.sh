4e043887ac8180c16df185facaa1da3640cf4716
nikostourvas <nikostourvas@gmail.com>
2023-05-30 21:01:22 +0300

    added todo section for varscan fpfilter

--- snp_indel_rm.sh
+++ snp_indel_rm.sh
@@ -2,6 +2,8 @@
 
 # declare variables
 RESULTS=../results/VCF
+REF=../data/reference/Qrob_PM1N.fa
+BAM_FILES=../results/align/*.sort.Q20.markdup.bam
 
 # Filter SNPs close to InDels
 java -jar /usr/share/java/varscan.jar filter $RESULTS/Qrob_total.snp.vcf \
@@ -9,3 +11,17 @@ java -jar /usr/share/java/varscan.jar filter $RESULTS/Qrob_total.snp.vcf \
     --min-coverage 50 --min-reads2 2 \
     --indel-file $RESULTS/Qrob_total.indel.vcf \
     --output-file $RESULTS/Qrob_total_filter.snp.vcf
+
+# OPTIONAL TODO!
+# Use VarScan false positive filter
+# The scientific basis of this filter is described in the VarScan 2 publication. It will improve
+#the precision of variant and mutation calling by removing artifacts associated with short-read alignment.
+#-For somatic mutations, generate bam-readcounts with the Tumor BAM. For LOH and Germline, generate readcounts with the Normal BAM
+#-For de novo mutations (trio calling), generate readcounts with the child BAM.
+# The filter requires the bam-readcount utility: https://github.com/genome/bam-readcount
+
+# Readcount BAM
+#bam-readcount -q 1 -b 20 -f $REF $BAM_FILES > $RESULTS/readcount.tsv
+
+#java -jar /usr/share/java/varscan.jar fpfilter $RESULTS/Qrob_total_filter.snp.vcf \
+#    [readcount file] OPTIONS
