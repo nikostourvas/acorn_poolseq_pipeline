20adaf58f0f5ceef1186b6d4a34e9b9352b8bcc2
nikostourvas <nikostourvas@gmail.com>
2023-07-03 04:22:24 +0300

    comments on master pipeline script

--- draft_pipeline.sh
+++ draft_pipeline.sh
@@ -4,31 +4,31 @@
 # Generate a hash for each file
 md5sum ../data/untrimmed_fastq/*.fastq.gz >  ../data/untrimmed_fastq/hashList.txt &&
 
-# fastqc
+# fastqc - Quality control for raw sequences
 # You can change number of cores per job by accessing fastqc.sh script.
 bash fastqc.sh untrimmed_fastq data &&
 
-# make list of samples
+# make list of samples to allow for parallel analysis of multiple samples
 for i in ../data/untrimmed_fastq/*_R1_001.fastq.gz
 do
 	echo $(basename ${i%_R*})
 done > ../data/inds &&
 
-# Trimmomatic
+# Trimmomatic - filter out bad reads or bad parts of a read
 # You can change number of cores per job by accessing trimmomatic_parallel.sh script.
 # For example you can set trimmomatic to use 4 cores. 
 # ATTN: So if you ask parallel to run 2 jobs concurrently, you need 2 x 4 = 8 cores.
 parallel --verbose -j 2 \
 	'bash trimmomatic_parallel.sh {}' :::: ../data/inds &&
 
-# fastqc again
+# fastqc again - Quality control for trimmed sequences
 # You can change number of cores per job by accessing fastqc.sh script.
 bash fastqc.sh trimmed_fastq results &&
 
 # Create a unified report with MultiQC
 multiqc ../results -o ../results &&
 
-# Map
+# Map reads to reference genome
 # You can change number of cores per job by accessing mapping.sh script.
 bwa index ../data/reference/Qrob_PM1N.fa &&
 samtools faidx ../data/reference/Qrob_PM1N.fa &&
@@ -39,7 +39,7 @@ parallel --verbose -j 2 \
 parallel --verbose -j 2 \
 	'bash bam_cleaning.sh {}' :::: ../data/inds &&
 
-# make list of all chromosomes & scaffolds
+# make list of all chromosomes & scaffolds for next step
 grep 'Qrob' ../data/reference/Qrob_PM1N.fa | \
  cut -c2- > ../data/regions && 
 
@@ -47,10 +47,14 @@ grep 'Qrob' ../data/reference/Qrob_PM1N.fa | \
 parallel --verbose -j 20 \
 	'bash variant_calling_parallel.sh {}' :::: ../data/regions &&
 
+# Merge the multiple small VCF files that were produced in the previous step
+# into one large VCF
 bash vcf_merge.sh &&
 
+# Filter SNPs detected close to INDELs, as suggested by VarScan manual
 bash snp_indel_rm.sh &&
 
+# Filter out multi-allelic SNPs
 bash vcf_biallelic.sh &&
 
 # Create a unified final report with MultiQC
