96555175cbe803cf54ff5f2834ad48778cfc1e97
nikostourvas <nikostourvas@gmail.com>
2023-07-18 09:21:43 +0200

    fix pool_sizes_sim

--- grenedalf_fst_genome.sh
+++ grenedalf_fst_genome.sh
@@ -5,7 +5,7 @@ mkdir -p ../results/frequencies
 # declare variables
 OUTPUT=../results/frequencies
 SYNC=../results/frequencies/simulate.sync.gz
-POOLSIZES=/mnt/workshop_backup/pool_sizes_sim
+POOLSIZES=pool_sizes_sim
 
 grenedalf fst \
     --window-type genome \
@@ -17,6 +17,6 @@ grenedalf fst \
     --filter-total-only-biallelic-snps \
     --sync-path $SYNC \
     --out-dir $OUTPUT \
-    --log-file $OUTPUT/grenedalf_fst_log.txt \
+    --log-file $OUTPUT/grenedalf_fst_genome_log.txt \
     --allow-file-overwriting \
     --threads 4
