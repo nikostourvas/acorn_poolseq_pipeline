cd5cdd96fdb7530a452fb891d977c11141c51fa6
Lars <138503061+LarsLittmann@users.noreply.github.com>
2023-08-11 12:12:24 +0200

    Simplify data input
    All information that we need is now extracted from a directory path.

--- fastqc.sh
+++ fastqc.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Variables
-READTYPE=$1 # either trimmed_fastq or untrimmed_fastq
-DIRECTORY=$2 # either /mnt/data or ..results
-# create output directory 
-mkdir -p ../results/fastqc_$1
+#This script takes the name of a directory that contains fastq files and outputs a new directory in results that contains the fastqc reports. 
+
+#Taking input
+DIRECTORY_TO_QC=$1 # The full path to the directory that contains the fastq files you would like to perform QC on. 
+
+# create output directory
+OUTPUT_DIRECTORY= ../results/fastqc_$(basename(${DIRECTORY_TO_QC})) #Use the name of the directory that stores the fastq files to name the output directory.
+mkdir -p ${OUTPUT_DIRECTORY} # Make sure that the output directory exists.
 
 # run fastqc
 # edit number of cores used with the "-t" option
-fastqc -t 4 -o ../results/fastqc_$1/ $2/$1/*fastq.gz
+fastqc -t 4 -o ${OUTPUT_DIRECTORY} ${DIRECTORY_TO_QC}/*fastq.gz
