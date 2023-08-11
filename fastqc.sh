1280048be813c1db382b53d3d67d1928809dd259
Lars <138503061+LarsLittmann@users.noreply.github.com>
2023-08-11 12:17:55 +0200

    fix typo

--- fastqc.sh
+++ fastqc.sh
@@ -6,7 +6,7 @@
 DIRECTORY_TO_QC=$1 # The full path to the directory that contains the fastq files you would like to perform QC on. 
 
 # create output directory
-OUTPUT_DIRECTORY= ../results/fastqc_$(basename(${DIRECTORY_TO_QC})) #Use the name of the directory that stores the fastq files to name the output directory.
+OUTPUT_DIRECTORY=../results/fastqc_$(basename(${DIRECTORY_TO_QC})) #Use the name of the directory that stores the fastq files to name the output directory.
 mkdir -p ${OUTPUT_DIRECTORY} # Make sure that the output directory exists.
 
 # run fastqc
