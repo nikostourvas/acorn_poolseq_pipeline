4502e33ba8132a0b03c3acc5ac1929098a59b9d2
nikostourvas <nikostourvas@gmail.com>
2023-08-21 14:01:07 +0300

    remove indexing as it is handled in upstream script

--- vcf_merge.sh
+++ vcf_merge.sh
@@ -4,33 +4,17 @@
 RESULTS=../results/VCF
 SNPVCFS=$RESULTS/*snp.vcf
 INDELVCFS=$RESULTS/*indel.vcf
+THREADS=12
 
-# some scaffolds will have no snps/indels
-# delete vcf files originating from these scaffolds
-find $RESULTS -maxdepth 1 -type f -empty -print -delete
+# Merge VCF files containing SNPs from each genomic region
+bcftools concat --threads ${THREADS} $RESULTS/Qrob*.snp.vcf.gz \
+    -o $RESULTS/Qrob_total.snp.vcf \
+    2> ${RESULTS}/${REGION}.bcftools.snp.merge.vcf.err
 
-# zip VCFs
-for SNPVCF in ${SNPVCFS[@]}
-do
-    bgzip -k $SNPVCF
-done
+# Merge VCF files containing INDELs from each genomic region
+bcftools concat --threads ${THREADS} $RESULTS/Qrob*.indel.vcf.gz \
+    -o $RESULTS/Qrob_total.indel.vcf \
+    2> ${RESULTS}/${REGION}.bcftools.indel.merge.vcf.err
 
-for INDELVCF in ${INDELVCFS[@]}
-do
-    bgzip -k $INDELVCF
-done
-
-# Index VCFs
-for SNPVCF in ${SNPVCFS[@]}
-do
-    bcftools index --threads 4 ${SNPVCF}.gz
-done
-
-for INDELVCF in ${INDELVCFS[@]}
-do
-    bcftools index --threads 4 ${INDELVCF}.gz
-done
-
-# Merge VCFs
-bcftools concat --threads 4 $RESULTS/Qrob*.snp.vcf.gz -o $RESULTS/Qrob_total.snp.vcf
-bcftools concat --threads 4 $RESULTS/Qrob*.indel.vcf.gz -o $RESULTS/Qrob_total.indel.vcf
+# Here it would be optimal to output compressed VCF files. However the
+# downstream 'varscan filter' command does not work with compressed files.
