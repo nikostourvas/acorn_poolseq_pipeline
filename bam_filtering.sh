e0c6611e37e4c7713ed71b05c76222dbc436c96b
Lars <138503061+LarsLittmann@users.noreply.github.com>
2023-08-25 12:11:55 +0200

    Address legibility concerns.
    Changed variant name from OUTPUT to BAM. Before, it looked like input and output were the same thing. By labeling it BAM, the reader knows what is being processed.

--- bam_filtering.sh
+++ bam_filtering.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # declare variables
-IND=${1}
-OUTPUT=../results/align_Batch1/${IND}
+IND=$1 #The individual that is processed
+BAM=../results/align_Batch1/${IND} #The location of the bam files, by individual.
 
 # Remove PCR and optical duplicates with samtools markdup
 # This is the recommended way to mimic Picard tools MarkDuplicates with samtools
@@ -12,7 +12,7 @@ OUTPUT=../results/align_Batch1/${IND}
 # Quick explanation of what individual commands do:
 # collate: groups the reads by read name in the bam file. This puts the read 
 # pairs close together so that fixmate can work 
-# fixmate: fix read-mates so that they both have the same sets of attributesfor
+# fixmate: fix read-mates so that they both have the same sets of attributes for
 # the subsequent preprocessing
 # sort: sort reads again based on genomic coordinates and
 # markdup: remove duplicates (-r argument)
@@ -24,21 +24,21 @@ OUTPUT=../results/align_Batch1/${IND}
 # -F 0x0004 remove reads that are not mapped
 # -F 0x0008 remove reads with an un-mapped mate
 # -F 0x100 remove secondary alignments
-samtools collate -O -u ${OUTPUT}.raw.bam 2> ${OUTPUT}.collate.err \
-    | samtools fixmate -m -u - - 2> ${OUTPUT}.fixmate.err \
-    | samtools sort -u - 2> ${OUTPUT}.sort.err\
-    | samtools markdup -u -r -f ${OUTPUT}_markdup_stats_file.txt \
-        -S -d 2500 --mode s --include-fails - - 2> ${OUTPUT}.markdup.err \
+samtools collate -O -u ${BAM}.raw.bam 2> ${BAM}.collate.err \
+    | samtools fixmate -m -u - - 2> ${BAM}.fixmate.err \
+    | samtools sort -u - 2> ${BAM}.sort.err\
+    | samtools markdup -u -r -f ${BAM}_markdup_stats_file.txt \
+        -S -d 2500 --mode s --include-fails - - 2> ${BAM}.markdup.err \
     | samtools view -b -q 20 \
         -f 0x0002 \
         -F 0x0004 \
         -F 0x0008 \
         -F 0x100 \
-        -o ${OUTPUT}.markdup.Q20.bam - 2> ${OUTPUT}.markdup-view.err
+        -o ${BAM}.markdup.Q20.bam - 2> ${BAM}.markdup-view.err
 
 # gather statistics
 # -@ number of cores
-samtools flagstat -@ 1 ${OUTPUT}.markdup.Q20.bam > ${OUTPUT}.markdup.Q20.flagstat
+samtools flagstat -@ 1 ${BAM}.markdup.Q20.bam > ${BAM}.markdup.Q20.flagstat
 
 # index bam files
-samtools index -@ 1 ${OUTPUT}.markdup.Q20.bam
+samtools index -@ 1 ${BAM}.markdup.Q20.bam
