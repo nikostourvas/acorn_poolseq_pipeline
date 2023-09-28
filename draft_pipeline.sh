73391cad46c5639c10945952aca3768f9f98a412
nikostourvas <nikostourvas@gmail.com>
2023-09-28 12:35:31 +0300

    more updates for Batch2

--- draft_pipeline.sh
+++ draft_pipeline.sh
@@ -10,21 +10,21 @@ md5sum /mnt/data/untrimmed_fastq/*.fastq.gz >  ../results/hashList.txt &&
 bash fastqc.sh untrimmed_fastq /mnt/data &&
 
 # make list of samples to allow for parallel analysis of multiple samples
-for i in /mnt/data/untrimmed_fastq/*_R1_001.fastq.gz
+for i in /mnt/AcornSeqdata/AdapterClipped_Batch2/Sample*/*_R1_clipped.fastq.gz
 do
 	echo $(basename ${i%_R*})
-done > /mnt/data/inds &&
+done > /mnt/AcornSeqdata/inds_Batch2.txt
 
 # Trimmomatic - filter out bad reads or bad parts of a read
 # You can change number of cores per job by accessing trimmomatic_parallel.sh script.
 # For example you can set trimmomatic to use 4 cores. 
 # ATTN: So if you ask parallel to run 2 jobs concurrently, you need 2 x 4 = 8 cores.
-parallel --verbose -j 2 \
-	'bash trimmomatic_parallel.sh {} 1>/mnt/results/trimmed_fastq_Batch1/logs/{}.out 2> /mnt/results/trimmed_fastq_Batch1/logs/{}.error' :::: /mnt/AcornSeqdata/inds_Batch1.txt &&
+parallel --verbose -j 36 \
+	'bash trimmomatic_parallel.sh {} 1>/mnt/results/trimmed_fastq_Batch2/logs/{}.out 2> /mnt/results/trimmed_fastq_Batch2/logs/{}.error' :::: /mnt/AcornSeqdata/inds_Batch2.txt
 
 # fastqc again - Quality control for trimmed sequences
 # You can change number of cores per job by accessing fastqc.sh script.
-bash fastqc.sh ../results/trimmed_fastq_Batch1 &&
+bash fastqc.sh ../results/trimmed_fastq_Batch2 &&
 
 # Create a unified report with MultiQC
 multiqc ../results -o ../results &&
@@ -35,10 +35,10 @@ bwa index /mnt/data/reference/Qrob_PM1N.fa &&
 samtools faidx /mnt/data/reference/Qrob_PM1N.fa &&
 
 parallel --verbose -j 12 \
-	'bash map.sh {}' :::: ../AcornSeqdata/inds_Batch1.txt
+	'bash map.sh {}' :::: ../AcornSeqdata/inds_Batch2.txt
 
 parallel --verbose -j 7 \
-	'bash bam_filtering.sh {}' :::: ../AcornSeqdata/inds_Batch1.txt
+	'bash bam_filtering.sh {}' :::: ../AcornSeqdata/inds_Batch2.txt
 
 # make list of all chromosomes & scaffolds for next step
 bash Simplified_Chunks.sh ../reference/Qrob_PM1N.fa.fai 2000000
