6821a355af7be272529f06a4fd0cb3033fb52997
nikostourvas <nikostourvas@gmail.com>
2023-09-29 11:38:18 +0300

    switch to single-threaded mapping

--- map.sh
+++ map.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # create output directory
-mkdir -p ../results/align_Batch1
+mkdir -p ../results/align_Batch2
 
 # declare variables
 IND=${1}
 REF=../reference/Qrob_PM1N.fa
-FORWARD=../results/trimmed_fastq_Batch1/${IND}_1.trim.fastq.gz
-REVERSE=../results/trimmed_fastq_Batch1/${IND}_2.trim.fastq.gz
+FORWARD=../results/trimmed_fastq_Batch2/${IND}_1.trim.fastq.gz
+REVERSE=../results/trimmed_fastq_Batch2/${IND}_2.trim.fastq.gz
 RG="@RG\tID:${IND}\tPL:Illumina\tSM:${IND}"
-OUTPUT=../results/align_Batch1/${IND}
+OUTPUT=../results/align_Batch2/${IND}
 
 # Align to reference genome and export raw bam file
 
@@ -23,10 +23,10 @@ OUTPUT=../results/align_Batch1/${IND}
 
 # Be mindful to account for as many cores have been assigned to bwa and samtools 
 # (e.g. 2c BWA + 1c samtools = 3 cores in total)
-# samtools flagstat is run afterwards (much sorter time) utilizing all 3 cores
+# samtools flagstat is run afterwards 
 
-bwa mem -R ${RG} -M -t 2 ${REF} ${FORWARD} ${REVERSE} 2> ${OUTPUT}.bwa-mem.err \
+bwa mem -R ${RG} -M -t 1 ${REF} ${FORWARD} ${REVERSE} 2> ${OUTPUT}.bwa-mem.err \
     | samtools view -h -b -o ${OUTPUT}.raw.bam 2> ${OUTPUT}.sam-view.err
 
 # gather statistics
-samtools flagstat -@ 3 ${OUTPUT}.raw.bam > ${OUTPUT}.raw.flagstat
+samtools flagstat -@ 1 ${OUTPUT}.raw.bam > ${OUTPUT}.raw.flagstat
