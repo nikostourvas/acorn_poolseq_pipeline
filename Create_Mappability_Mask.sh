624ea3399cf3a243af82c5952f71d2d44fad3c4d
LarsLittmann <larslittmann@gmail.com>
2023-10-11 15:29:26 +0200

    Continued testing. Correct directories for maks generation

--- Create_Mappability_Mask.sh
+++ Create_Mappability_Mask.sh
@@ -7,15 +7,15 @@
 #The only required input is a reference genome. Another thing to consider is the read length. (150 here, but may need to be changed depending on the application)
 
 # refgenome as first input
-GENOME=../reference/Qrob_PM1N.fa
+GENOME=/mnt/reference/Qrob_PM1N.fa
 # outdir as second
-OUTDIR=../reference/SNPability_Mask
+OUTDIR=/mnt/reference/SNPability_Mask
 #Temporary directory for intermediate files
-INTDIR=../reference/SNPability_Mask/Intermediates
+INTDIR=/mnt/reference/SNPability_Mask/Intermediates
 # set the path to where the seqbility scripts are
-SEQBILITY_PATH=../reference/SNPability_Mask/seqbility-20091110
+SEQBILITY_PATH=/mnt/reference/SNPability_Mask/seqbility-20091110
 # tools to msmc-tools (used in the last step)
-MSMC_TOOLS_PATH=../reference/SNPability_Mask/msmc-tools
+MSMC_TOOLS_PATH=/mnt/reference/SNPability_Mask/msmc-tools
 
 # create outdir if it doesn't exist
 mkdir -p ${OUTDIR}
@@ -24,18 +24,18 @@ mkdir -p ${OUTDIR}
 mkdir -p ${INTDIR}
 
 # split the refgenome in to kmers as artificial reads, using 150 since that's our readlenght
-${SEQBILITY_PATH}/splitfa ${GENOME} 150 > ${INTDIR}/$(basename ${GENOME})_150splits.fa
+#${SEQBILITY_PATH}/splitfa ${GENOME} 150 > ${INTDIR}/$(basename ${GENOME})_150splits.fa
 # map them back to the reference chromosome
-bwa aln -t 2 -R 1000000 -O 3 -E 3 ${GENOME} ${INTDIR}/$(basename ${GENOME})_150splits.fa 2>${INTDIR}/Mappability_BWAaln.log > ${INTDIR}/$(basename ${GENOME})_150splits.aln.sai
+#bwa aln -t 2 -R 1000000 -O 3 -E 3 ${GENOME} ${INTDIR}/$(basename ${GENOME})_150splits.fa 2>${INTDIR}/Mappability_BWAaln.log > ${INTDIR}/$(basename ${GENOME})_150splits.aln.sai
 # convert sai to sam
-bwa samse ${GENOME} ${INT_DIR}/$(basename ${GENOME})_150splits.aln.sai ${INTDIR}/$(basename ${GENOME})_150splits.fa 2>${INTDIR}/Mappability_BWAsamse.log > ${INTDIR}/$(basename ${GENOME})_150splits.aln.sam
+#bwa samse ${GENOME} /mnt/reference/SNPability_Mask/Intermediates/Qrob_PM1N.fa_150splits.aln.sai ${INTDIR}/$(basename ${GENOME})_150splits.fa 2>${INTDIR}/Mappability_BWAsamse.log > ${INTDIR}/$(basename ${GENOME})_150splits.aln.sam
 # compress it
-bgzip ${INTDIR}/$(basename ${GENOME})_150splits.aln.sam
+#bgzip ${INTDIR}/$(basename ${GENOME})_150splits.aln.sam 2> ${INTDIR}/bgzip.log > ${INTDIR}/$(basename ${GENOME})_150splits.aln.sam.gz
 # generate the raw mask file
-gzip -dc ${INTDIR}/$(basename ${GENOME})_150splits.aln.sam.gz | seqbility/seqbility-20091110/gen_raw_mask.pl > ${INTDIR}/rawMask_$(basename ${GENOME})_150.fa
+gzip -dc ${INTDIR}/$(basename ${GENOME})_150splits.aln.sam.gz 2>${INTDIR}/Decompress.log | ${SEQBILITY_PATH}/gen_raw_mask.pl > ${INTDIR}/rawMask_$(basename ${GENOME})_150.fa
 # convert step 1
 mkdir -p ${OUTDIR}/mappability_masks
-${SEQBILITY_PATH}/seqbility-20091110/gen_mask -l 150 -r 0.5 ${INTDIR}/rawMask_$(basename ${GENOME})_150.fa > ${OUTDIR}/mappability_masks/mask_$(basename ${GENOME})_150.fa
+${SEQBILITY_PATH}/gen_mask -l 150 -r 0.5 ${INTDIR}/rawMask_$(basename ${GENOME})_150.fa > ${OUTDIR}/mappability_masks/mask_$(basename ${GENOME})_150.fa
 # convert step 2, using the python script from msmc tools. Modified this to take input and output as command line arguments, for easier automation
 # IMPORTANT: this script was written in python 2x something, it will not work under python3!
 
