9dc8c2297d13e41c75062122c292990f04773e8a
Lars <138503061+LarsLittmann@users.noreply.github.com>
2023-10-19 13:52:35 +0200

    Update all variable names and take input from command line

--- vcf_biallelic.sh
+++ vcf_biallelic.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # declare variables
-RESULTS=../results/VCF
+INPUT_VCF=$1
 REF=../reference/Qrob_PM1N.fa
 THREADS=12
+OUTDIR=$(dirname ${INPUT_VCF})
 
 # keep only biallelic SNPs
 bcftools view --threads ${THREADS} -m2 -M2 -v snps \
-    $RESULTS/Qrob_total_filter.snp.vcf \
-    -Oz -o $RESULTS/Qrob_total_filter2.snp.vcf.gz \
-    2> ${RESULTS}.bcftools.biallelic.vcf.err
+    ${INPUT_VCF} \
+    -Oz -o ${OUTDIR}/$(basename ${INPUT_VCF/.vcf/_Biallelic.vcf.gz}) \
+    2> ${OUTDIR}/bcftools_biallelic_vcf.err
 
 # produce statistics
-bcftools stats --threads ${THREADS} --fasta-ref $REF \
-    $RESULTS/Qrob_total_filter2.snp.vcf.gz \
-    > $RESULTS/bcftools_stats.txt
+bcftools stats --threads ${THREADS} --fasta-ref ${REF} \
+    ${OUTDIR}/$(basename ${INPUT_VCF/.vcf/_Biallelic.vcf.gz}) \
+    > ${OUTDIR}/$(basename ${INPUT_VCF/.vcf/_bcftools_stats.txt})
