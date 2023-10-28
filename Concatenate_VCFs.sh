bbda163b0813f6ef494d59484de1023d7a8ad069
nikostourvas <nikostourvas@gmail.com>
2023-10-28 23:21:53 +0300

    concatenate to uncompressed VCF as needed for varscan, add standard error files, add multithreading

--- Concatenate_VCFs.sh
+++ Concatenate_VCFs.sh
@@ -8,6 +8,7 @@
 
 UNMERGED_VCF_DIR=$1 #The path to the directory in which all the unmerged VCF files are stored
 VCF_PREFIX=$2 #The name for the merged VCF
+THREADS=$3 #The number of CPU cores to use
 
 #Create a list of filepaths containing each of the VCFs we wish to merge. One for SNPs, one for Indels
 realpath ${UNMERGED_VCF_DIR}/chunk*.varScan.snp.vcf.gz | sort -V > ${UNMERGED_VCF_DIR}/SNP_VCF_List.txt
@@ -17,12 +18,16 @@ realpath ${UNMERGED_VCF_DIR}/chunk*.varScan.indel.vcf.gz | sort -V > ${UNMERGED_
 #This implementation does not allow for overlaps
 #-f takes input from the list of filepaths we created earlier.
 #-a allows for discontiguous regions to be accepted (manual check: This only applies to the very edges of chromosomes. No harm there).
-#-O z sets the output type to gzipped vcf
+#-O z sets the output type to gzipped vcf, and v to uncompressed vcf
 #-o is the output prefix (specified by user)
-bcftools concat -f ${UNMERGED_VCF_DIR}/SNP_VCF_List.txt -a -O z -o ${UNMERGED_VCF_DIR}/${VCF_PREFIX}_SNP.vcf.gz
+bcftools concat -f ${UNMERGED_VCF_DIR}/SNP_VCF_List.txt \
+    -a -O v -o ${UNMERGED_VCF_DIR}/${VCF_PREFIX}_SNP.vcf \
+    --threads ${THREADS} 2> ${UNMERGED_VCF_DIR}/${VCF_PREFIX}_SNP.vcf.err
 
 #Concatenate the VCFs containing INDELs using bcftools.
-bcftools concat -f ${UNMERGED_VCF_DIR}/INDEL_VCF_List.txt -a -O z -o ${UNMERGED_VCF_DIR}/${VCF_PREFIX}_INDEL.vcf.gz
+bcftools concat -f ${UNMERGED_VCF_DIR}/INDEL_VCF_List.txt \
+    -a -O v -o ${UNMERGED_VCF_DIR}/${VCF_PREFIX}_INDEL.vcf \
+    --threads ${THREADS} 2> ${UNMERGED_VCF_DIR}/${VCF_PREFIX}_INDEL.vcf.err
 
 #Remove some intermediate files
 rm ${UNMERGED_VCF_DIR}/SNP_VCF_List.txt
