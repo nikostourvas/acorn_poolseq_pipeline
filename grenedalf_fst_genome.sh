ed5ed0233fca24403aea26b72d71a7bf137f9450
nikostourvas <nikostourvas@gmail.com>
2023-11-14 14:03:45 +0200

    minor changes

--- grenedalf_fst_genome.sh
+++ grenedalf_fst_genome.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # declare variables
-TABLE=
+TABLE=/data/genetics_tmp/VCF_AllPools_3Species/VCF_AllPools_3SpeciesIndelFilteredSNPs_Biallelic_grenedalf.txt
 POOLSIZES=/data/genetics_tmp/variant_calling_tmp_storage_all_pools/Allpools_3species_poolsizes.txt
 REFERENCE=/mnt/reference/Qrob_PM1N.fa
-OUTDIR=/data/genetics_tmp/grenedalf_3species
+OUTDIR=/data/genetics_tmp/grenedalf_Allpools_3species
 THREADS=12
 
 mkdir -p ${OUTDIR} 
@@ -15,7 +15,7 @@ grenedalf fst \
     --pool-sizes $OUTDIR/$POOLSIZES \
     --filter-sample-min-count 8 \
     --filter-sample-max-coverage 500 \
-    --filter-sample-min-coverage 30 \
+    --filter-sample-min-coverage 40 \
     --filter-total-only-biallelic-snps \
     --reference-genome-fasta-file $REFERENCE\
     --vcf-path $TABLE \
