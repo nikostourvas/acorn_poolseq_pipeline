a68ad91d3c9ee48218af2f6f5f016e8b4f68313d
nikostourvas <nikostourvas@gmail.com>
2023-11-14 14:02:17 +0200

    changes to work with table as input

--- grenedalf_fst_genome.sh
+++ grenedalf_fst_genome.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # declare variables
-OUTDIR=/data/genetics_tmp/grenedalf_3species
-VCF=/data/genetics_tmp/VCF_AllPools_3Species/VCF_AllPools_3SpeciesIndelFilteredSNPs_Biallelic.vcf.gz
+TABLE=
 POOLSIZES=/data/genetics_tmp/variant_calling_tmp_storage_all_pools/Allpools_3species_poolsizes.txt
 REFERENCE=/mnt/reference/Qrob_PM1N.fa
+OUTDIR=/data/genetics_tmp/grenedalf_3species
 THREADS=12
 
 mkdir -p ${OUTDIR} 
@@ -18,7 +18,7 @@ grenedalf fst \
     --filter-sample-min-coverage 30 \
     --filter-total-only-biallelic-snps \
     --reference-genome-fasta-file $REFERENCE\
-    --vcf-path $VCF \
+    --vcf-path $TABLE \
     --out-dir $OUTDIR \
     --log-file $OUTDIR/grenedalf_fst_genome_log.txt \
     --allow-file-overwriting \
