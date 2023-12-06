0a08d235464d2b2dc897dc34c11c0125a0aacff6
LarsLittmann <larslittmann@gmail.com>
2023-12-06 14:46:11 +0000

    LOTS of trial and error testing to correctly format allele frequency table

--- vcf_to_grenedalf_table.sh
+++ vcf_to_grenedalf_table.sh
@@ -14,7 +14,10 @@
 
 # declare variables
 VCF=${1}
-OUTPUT=${2}
+
+OUTPUT=${VCF/.vcf/}
+
+N_SAMPLES=$(bcftools query -l ${VCF} | wc -l)
 
 # bcftools query arguments (extract data from VCF)
 # -H: include header
@@ -30,6 +33,31 @@ bcftools query -Hf '%CHROM\t%POS\t%REF\t%ALT[\t%RD][\t%AD]\n' ${VCF} \
             };
         print; next} 
         NR>1 {for (i=2; i<=NF; i++) 
-            gsub(/\./, 0, $i); print
+            gsub(/\./, NA, $i); print
         }' \
-    > ${OUTPUT}.txt
+| sed -e 's/\t\t/\tNA\t/g' > ${OUTPUT}_intermediate.txt
+
+n=0
+
+while [ ${n} -le ${N_SAMPLES} ]; do
+RD_POS=$((5+${n}));
+AD_POS=$((${RD_POS}+${N_SAMPLES}));
+FREQ_POS=$((${AD_POS}+${N_SAMPLES}));
+awk -F '\t' -v awkRDpos="${RD_POS}" -v awkADpos="${AD_POS}" -v awkFREQpos="${FREQ_POS}" \
+' OFS = "\t" {awkFREQvalue=$awkADpos/($awkRDpos+$awkADpos); print $0,  $awkFREQpos=awkFREQvalue }' ${OUTPUT}_intermediate.txt > \
+${OUTPUT}_buffer.txt;
+cat ${OUTPUT}_buffer.txt > ${OUTPUT}_intermediate.txt;
+echo ${n};
+n=$(( ${n}+1 ));
+done
+
+awk -F "\t" ' OFS = "\t" {chrompos=$1"_"$2; print chrompos, $0} ' ${OUTPUT}_intermediate.txt > ${OUTPUT}_big.txt
+cut -f1,$((6+(2*${N_SAMPLES})))-$((6+(3*${N_SAMPLES}))) ${OUTPUT}_big.txt > ${OUTPUT}_small.txt
+
+head -n 1 ${OUTPUT}_big.txt | cut -f1,6-$((6+${N_SAMPLES})) | sed -e 's/P01-...-ACORN-BOKU-...-//g' |\
+sed -e 's/.ref.cnt//g' > ${OUTPUT}_AlleleFrequencyTable.txt
+
+sed -e 's/-nan/NA/g' ${OUTPUT}_small.txt | tail +2 >> ${OUTPUT}_AlleleFrequencyTable.txt
+
+rm ${OUTPUT}_buffer.txt
+rm ${OUTPUT}_intermediate.txt
