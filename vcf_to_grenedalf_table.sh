f2b4a892b2ab62f13b6e8b237d3d8b20505b3c20
LarsLittmann <larslittmann@gmail.com>
2024-01-12 14:01:04 +0000

    Fix problems caused by bad annotation. Fix bug that adds 1 column too many

--- vcf_to_grenedalf_table.sh
+++ vcf_to_grenedalf_table.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Nikos Tourvas & Lars Littmann   
+# Nikos Tourvas & Lars Littmann 
 # 2023.11.14
 # Take a VCF produced by VarScan and output a table containing the allele frequencies for all samples per position.
 
 # declare variables
 VCF=${1}
 
-OUTPUT=${VCF/.vcf/} #Take the name of the input vcf without file type suffix.
+#Take the name of the input vcf without file type suffix.
+OUTPUT=${VCF/.vcf.gz/}
 
-N_SAMPLES=$(bcftools query -l ${VCF} | wc -l) #Extract the number of samples found in the vcf.
+#Extract the number of samples found in the vcf.
+N_SAMPLES=$(bcftools query -l ${VCF} | wc -l)
 
 # We extract the following information from the VCF: 
 # Chromosome, Position, Ref Allele, Alt Allele, Ref Allele read depth (per pool), Alt Allele read depth (per pool)
@@ -23,11 +25,11 @@ N_SAMPLES=$(bcftools query -l ${VCF} | wc -l) #Extract the number of samples fou
 #                     (c) replace the suffix ":AD" with ".alt.cnt" in columns which signify alt allele counts
 # 2.Replace periods (.) which signify missing data with zeros (0) in the allele count columns
 
-bcftools query -Hf '%CHROM\t%POS\t%REF\t%ALT[\t%RD][\t%AD]\n' ${VCF} \ #This line is responible for the data extraction
-    | awk 'BEGIN{OFS=FS="\t"} 
+bcftools query -Hf '%CHROM\t%POS\t%REF\t%ALT[\t%RD][\t%AD]\n' ${VCF}\
+   | awk 'BEGIN{OFS=FS="\t"} 
         NR==1 {
-            $1="chrom"; $2="pos"; $3="ref"; $4="alt"; #Name columns 
-            for (i=5; i<=NF; i++){ #Change the format of the vcf fields to something that is easier to edit.
+            $1="chrom"; $2="pos"; $3="ref"; $4="alt";
+            for (i=5; i<=NF; i++){
                 gsub(/\[[^]]*\]/, "", $i); 
                 gsub(/:RD$/, ".ref.cnt", $i); 
                 gsub(/:AD$/, ".alt.cnt", $i)
@@ -36,7 +38,8 @@ bcftools query -Hf '%CHROM\t%POS\t%REF\t%ALT[\t%RD][\t%AD]\n' ${VCF} \ #This lin
         NR>1 {for (i=2; i<=NF; i++) 
             gsub(/\./, NA, $i); print
         }' \
-| sed -e 's/\t\t/\tNA\t/g' > ${OUTPUT}_intermediate.txt #Make sure that empty fields become 'NA'.
+| sed -e 's/\t\t/\tNA\t/g' > ${OUTPUT}_intermediate.txt
+ #Make sure that empty fields become 'NA'.
 
 #Calculate the allele frequencies using the pairs of RD and AD fields
 #The intermediate table is structured such that for each genomic position, there are a few columns with position information.
@@ -48,12 +51,10 @@ bcftools query -Hf '%CHROM\t%POS\t%REF\t%ALT[\t%RD][\t%AD]\n' ${VCF} \ #This lin
 n=0 #Call a variable to store how many times the loop has run
 
 while [ ${n} -le ${N_SAMPLES} ]; do #Run the loop as many times as there are samples in the VCF.
-RD_POS=$((5+${n}));#Tell awk in which column to find an RD value
-AD_POS=$((${RD_POS}+${N_SAMPLES}));#Tell awk in which column to find an AD value
+RD_POS=$((5+${n}));
+AD_POS=$((${RD_POS}+${N_SAMPLES}));
 FREQ_POS=$((${AD_POS}+${N_SAMPLES})); #Tell awk in which column to place the allele frequency it calculates.
-awk -F '\t' -v awkRDpos="${RD_POS}" -v awkADpos="${AD_POS}" -v awkFREQpos="${FREQ_POS}" \ # Pass the variants we created on to awk
-' OFS = "\t" {awkFREQvalue=$awkADpos/($awkRDpos+$awkADpos); print $0,  $awkFREQpos=awkFREQvalue }' ${OUTPUT}_intermediate.txt > \ #Calculate the allele frequency and place it in its corresponding column.
-${OUTPUT}_buffer.txt; #Write to an intermediate file so that awk does not try to write to the same file it is reading from
+awk -F '\t' -v awkRDpos="${RD_POS}" -v awkADpos="${AD_POS}" -v awkFREQpos="${FREQ_POS}" ' OFS = "\t" {awkFREQvalue=$awkADpos/($awkRDpos+$awkADpos); print $0,  $awkFREQpos=awkFREQvalue }' ${OUTPUT}_intermediate.txt > ${OUTPUT}_buffer.txt; #Write to an intermediate file so that awk does not try to write to the same file it is reading from
 cat ${OUTPUT}_buffer.txt > ${OUTPUT}_intermediate.txt; #Replace the table with the intermediate table we just created.
 echo ${n}; #print progress
 n=$(( ${n}+1 )); #update how many times the loop has run
@@ -62,10 +63,9 @@ done
 #Several final refinements to the table are necessary
 
 awk -F "\t" ' OFS = "\t" {chrompos=$1"_"$2; print chrompos, $0} ' ${OUTPUT}_intermediate.txt > ${OUTPUT}_big.txt #create a column that describes the genomic position in just one cell
-cut -f1,$((6+(2*${N_SAMPLES})))-$((6+(3*${N_SAMPLES}))) ${OUTPUT}_big.txt > ${OUTPUT}_small.txt #Extract only the columns that summarise the genomic position and all the allele frequencies.
+cut -f1,$((6+(2*${N_SAMPLES})))-$((5+(3*${N_SAMPLES}))) ${OUTPUT}_big.txt > ${OUTPUT}_small.txt #Extract only the columns that summarise the genomic position and all the allele frequencies.
 
-head -n 1 ${OUTPUT}_big.txt | cut -f1,6-$((6+${N_SAMPLES})) | sed -e 's/P01-...-ACORN-BOKU-...-//g' |\ #Create a new header with only the sample numbers (ACORN specific)
-sed -e 's/.ref.cnt//g' > ${OUTPUT}_AlleleFrequencyTable.txt
+head -n 1 ${OUTPUT}_big.txt | cut -f1,6-$((5+${N_SAMPLES})) | sed -e 's/P01-...-ACORN-BOKU-...-//g' | sed -e 's/.ref.cnt//g' > ${OUTPUT}_AlleleFrequencyTable.txt
 
 sed -e 's/-nan/NA/g' ${OUTPUT}_small.txt | tail +2 >> ${OUTPUT}_AlleleFrequencyTable.txt #Make sure that NAs are noted correctly.
 
