459ec2a26f21187fba90093549421424c1ddf483
nikostourvas <nikostourvas@gmail.com>
2024-01-16 18:28:55 +0200

    set optical dup param

--- picard_markdup.sh
+++ picard_markdup.sh
@@ -7,7 +7,8 @@ DEDUP_BAM=${2}
 # remove duplicates with picard MarkDuplicates
 java -jar -Xms256m -Xmx27g ${JAVA_DIR}/picard.jar MarkDuplicates \
 	I=${BAM} O=${DEDUP_BAM} M=${DEDUP_BAM}.picardstat \
-	MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 \
+	OPTICAL_DUPLICATE_PIXEL_DISTANCE=2500 \
+    VALIDATION_STRINGENCY=SILENT \
 	REMOVE_DUPLICATES=true
 
 # gather statistics with samtools flagstat    
