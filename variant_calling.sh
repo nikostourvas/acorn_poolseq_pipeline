12d35e8730b03a0673f3392e2d94b2bbbf0ca9de
nikostourvas <nikostourvas@gmail.com>
2024-02-04 21:11:46 +0200

    minor fix

--- variant_calling.sh
+++ variant_calling.sh
@@ -2,7 +2,7 @@
 
 # declare variables
 BAM_LIST=/data/genetics_tmp/results/vcf/Qrobur_PoolBams_Sorted_FastStorage.txt #For downstream convenience, we recommend that this list of bams is sorted in a logical order.
-OUTDIR=/data/genetics_tmp/vcf/VCF_Qrobur_nomarkdup
+OUTDIR=/data/genetics_tmp/results/vcf/VCF_Qrobur_nomarkdup
 REF=/data/genetics_tmp/REFERENCE/Qrob_PM1N_Organelles.fa
 CHUNK=$1
 CHUNK_SHORT=$(basename ${CHUNK/.bed/})
