a5b6cb8c690ca463e76698e83354363f2df423d8
nikostourvas <nikostourvas@gmail.com>
2024-02-10 23:48:54 +0200

    transform counts to treemix format

---
+++ genetic_structure/counts2treemix.R
@@ -0,0 +1,27 @@
+library(dplyr)
+
+# Read the original data
+counts <- read.table("data/genetics_tmp/results/vcf/VCF_AllPools_Outgroups/VCF_AllPools_OutgroupsIndelFilteredSNPs_Biallelic_no-miss_ADP100_thin-15000_counts.txt",
+                     sep = ",", header = TRUE)
+original_data <- counts[ ,-c(1:2)]
+
+# List of unique pool IDs (e.g., "Pool_102")
+pool_ids <- unique(sub("(.ref|.alt)$", "", colnames(original_data)))
+
+# Initialize an empty data frame with the same number of rows as the original data
+result <- data.frame(matrix(ncol = length(pool_ids), nrow = nrow(original_data)))
+
+# Name the columns of the result data frame
+colnames(result) <- pool_ids
+
+# Loop through each pool ID and merge the .ref and .alt columns
+for (pool_id in pool_ids) {
+  result[[pool_id]] <- paste(original_data[[paste0(pool_id, ".ref")]], 
+                             original_data[[paste0(pool_id, ".alt")]], 
+                             sep = ",")
+}
+
+# Write the result to a new file
+write.table(result, "data/genetics_tmp/results/vcf/VCF_AllPools_Outgroups/VCF_AllPools_OutgroupsIndelFilteredSNPs_Biallelic_no-miss_ADP100_thin-15000_counts.txt", 
+            quote = FALSE, row.names = FALSE)
+
