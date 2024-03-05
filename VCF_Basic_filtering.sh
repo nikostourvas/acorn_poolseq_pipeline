20f14a9edd35fec0e336f25c2916a49ca835abf3
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-03-05 11:26:54 +0100

    Remove periods that are not part of suffix from file names.

--- VCF_Basic_filtering.sh
+++ VCF_Basic_filtering.sh
@@ -15,7 +15,7 @@
 
 VCF_IN=$1 #VCF can be either gzipped or full-fat
 MIN_RD=$2 #The minimum read depth. Integer
-MISSINGNESS=$3 # The maximum missingness allowed for a site, expressed as a decimal (ex. 0.1)
+MISSINGNESS=$3 # The maximum missingness allowed for a site, expressed as a decimal (example 0.1)
 MAF=$4 #The minimum allele frequency for a SNP to be included, expressed as a decimal (example 0.05)
 
 #Specify further variables
@@ -24,14 +24,12 @@ HD_MASK=/data/genetics_tmp/REFERENCE/MASKS/HDplot_Mask_D10_H06_Window5.bed
 FILENAME=$(basename ${VCF_IN/.vcf.gz/})
 OUT_DIR=$(dirname ${VCF_IN})
 INT_DIR=${OUT_DIR}/IntermediateFiles_${FILENAME}
-MISSINGNESS_STRING="F_MISSING>${MISSINGNESS}" #This is necessary because bcftools does not allow you to place a variable between quotes, but echo does.
-MAF_STRING="MAX(AD/DP)>=${MAF} & MIN(AD/DP)<=$(echo ${MAF} | awk '{print int(1-$1)}')"
-
-echo ${MISSINGNESS_STRING}
-echo ${MAF_STRING}
+MISSINGNESS_STRING="F_MISSING>${MISSINGNESS}" #bcftools does not allow you to place a variable inside a string. The solution is to place the variable in a pre-made string, and refering to that instead.
+MAF_STRING="MAX(AD/DP)>=${MAF} & MIN(AD/DP)<=$(echo ${MAF} | awk '{print int(1-$1)}')" #Same problem as the line above.
+MAF_FILE_SRING=$(echo ${MAF} | sed -e 's/\.//g') #Make a string that does not include a '.' to create clean file names.
+MISSINGNESS_FILE_STRING=$(echo ${MISSINGNESS} | sed -e 's/\.//g') #Make a string that does not include a '.' to create clean file names.
 
 #Create intermediate directory
-
 mkdir -p ${INT_DIR}
 
 #Change the version 4.3 vcf produced by VarScan to a 4.2 vcf.
@@ -60,20 +58,20 @@ vcftools --vcf ${INT_DIR}/${FILENAME}_V42.vcf --minDP ${MIN_RD} --max-meanDP ${M
 
 #Next up, we filter out sites with poor quality (which won't filter out much), and filter for missingness using bcftools
 bcftools +setGT ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}.recode.vcf -- -t q -n . -i 'GQ<15' | \
-bcftools filter -e "${MISSINGNESS_STRING}" > ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS}.vcf
+bcftools filter -e "${MISSINGNESS_STRING}" > ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS_FILE_STRING}.vcf
 
 # Filter for MAF>=5% in at least one population in the data set
-bcftools view ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS}.vcf \
+bcftools view ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS_FILE_STRING}.vcf \
 -i "${MAF_STRING}" -m2 \
-> ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS}_MAF${MAF}.vcf
+> ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS_FILE_STRING}_MAF${MAF_FILE_STRING}.vcf
 
 #Create a .vcf file that contains only a header, and let bedtools write to this file. Bedtools doesn't output a header (for some reason).
-grep '#' ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS}_MAF${MAF}.vcf \
-> ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS}_MAF${MAF}_HDplotMask.vcf
+grep '#' ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS_FILE_STRING}_MAF${MAF_FILE_STRING}.vcf \
+> ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS_FILE_STRING}_MAF${MAF_FILE_STRING}_HDplotMask.vcf
 
 #Finally, apply a mask created using HDplot output, that removes sites with excess heterozygosity.
-bedtools subtract -a ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS}_MAF${MAF}.vcf -b ${HD_MASK} \
->> ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS}_MAF${MAF}_HDplotMask.vcf
+bedtools subtract -a ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS_FILE_STRING}_MAF${MAF_FILE_STRING}.vcf -b ${HD_MASK} \
+>> ${INT_DIR}/${FILENAME}_MinDP${MIN_RD}_MaxMeanDP${MAX_RD_INT}_Missingness${MISSINGNESS_FILE_STRING}_MAF${MAF_FILE_STRING}_HDplotMask.vcf
 
 #As I was writing this script, I got insanely distracted at some point and made this chicken. Enjoy.
 
