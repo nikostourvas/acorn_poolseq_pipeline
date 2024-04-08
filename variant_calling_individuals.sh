8026d9de4929bd06934671860f3159b282f00749
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-04-08 14:23:14 +0200

    Add documentation and instructions.

--- variant_calling_individuals.sh
+++ variant_calling_individuals.sh
@@ -2,20 +2,22 @@
 #2023.08.23
 #Call variants (SNPs and INDELs) from BAMs that contain INDIVIDUAL sequencing data (not pool sequenced).
 #For the ACORN project, these individuals are 2x20 Quercus robur trees belonging to populations 305 and 312
-#This script is designed to run in parallel for regions of the genome.
-#In the case of the ACORN project, these regions are arbitrarily made 'Chunks' of roughly equal size.
-#Much of this script was adapted from Nikolaos Tourvas' script 'variant_calling.sh', part of the acorn poolseq pipeline
+#This script is optimised for working on arbitrary, smaller subsections of a genome (Chunks). See the Simplified_Chunks.sh script.
+#https://github.com/nikostourvas/acorn_poolseq_pipeline/blob/singularity/Simplified_Chunks.sh
+#This variant calling script does not output one complete vcf, but one vcf per specified chunk. 
+#Though this script is very useful for massive parallelisation, it requires a bit more manual manipulation between runs.
+#Some variables need to be changed within the script before initiating a new variant calling event. These variables have been marked UPDATE BEFORE RUNNING
+#A parallel command that should be run in the command line is included at the bottom of this document. 
 
 BAM_LIST_IND=/data/genetics_tmp/VCF_Qpubescens_Pools/Qpubescens_poolBams_sorted_FastStorage.txt #For downstream convenience, we recommend that this list of bams is sorted in a logical order.
-OUTDIR=/data/genetics_tmp/VCF_Qpubescens_Pools/
-REF=/mnt/reference/Qrob_PM1N.fa
-CHUNK=$1
+OUTDIR=/data/genetics_tmp/VCF_Qpubescens_Pools/ #Specify an output directory. UPDATE BEFORE RUNNING
+REF=/mnt/reference/Qrob_PM1N.fa #Location of the reference genome that raw reads were aligned to.
+CHUNK=$1 #Chunk that the current instance of the script is running on. Specified in command line. 
 CHUNK_SHORT=$(basename ${CHUNK/.bed/})
 THREADS=1
 
 #Make sure the output directory is created.
 mkdir -p ${OUTDIR}
-
 
 #Create a .txt file that contains the file names (without directory information or suffixes) found in the BAM_LIST.
 #First, create a directory to store the sample naming lists in.
@@ -111,6 +113,7 @@ bcftools index ${OUTDIR}/${CHUNK_SHORT}_ind.varScan.indel.vcf.gz \
     --threads ${THREADS} \
     2> ${OUTDIR}/${CHUNK_SHORT}_ind.bcftools_index.indel.vcf.err &&
 
+#The following commands are used to output a single, well-formatted log file for each of the chunks, to avoid overcrowding directories.
 echo -e "All log files for ${CHUNK_SHORT/.bed/}\n\n#####\n\nSamtools mpileup\n\n">${OUTDIR}/AllLogFiles_${CHUNK_SHORT}.log
 cat ${OUTDIR}/${CHUNK_SHORT}_ind.mpileup.err >> ${OUTDIR}/AllLogFiles_${CHUNK_SHORT}.log
 echo -e "\n\n#####\n\nvarScan.snpindel\n\n" >> ${OUTDIR}/AllLogFiles_${CHUNK_SHORT}.log
@@ -126,5 +129,15 @@ cat ${OUTDIR}/${CHUNK_SHORT}_ind.bcftools_index.snp.vcf.err >> ${OUTDIR}/AllLogF
 echo -e "\n\n#####\n\nbcftools index snp\n\n" >> ${OUTDIR}/AllLogFiles_${CHUNK_SHORT}.log
 cat ${OUTDIR}/${CHUNK_SHORT}_ind.bcftools_index.indel.vcf.err >> ${OUTDIR}/AllLogFiles_${CHUNK_SHORT}.log &&
 
+#Remove intermediate files to avoid overcrowding directories.
 rm ${OUTDIR}/${CHUNK_SHORT}*.err 
 rm ${OUTDIR}/${CHUNK_SHORT}_samtools.mpileup
+
+########################################################################
+#A handy way to run this script is as follows:
+#Create a .txt file that contains all the sample names of samples you wish to process. These sample names have to match with your file naming. 
+#Run the following parallel command. It initiates this bash script for each sample. As soon as one script finishes running, it starts the next until it has cycled through all file names in the .txt file.
+#These commands are not part of the script, but can be copied into the command line to run the script. 
+
+#parallel --verbose -j 80 \
+#	'bash /mnt/acorn_poolseq_pipeline/variant_calling_individuals.sh {}' :::: /data/genetics_tmp/REFERENCE/ChunkFiles/Locations_Of_Chunk_Beds.txt
