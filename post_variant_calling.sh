4591d67b05577a17c5ba96eab2ee38deb36873eb
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-04-08 15:05:18 +0200

    Add documentation

--- post_variant_calling.sh
+++ post_variant_calling.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#Nikos Tourvas & Lars Littmann
+#2023
 #This script takes chunks of a whole-genome vcf that are placed together in a directory. They are merged and transformed to a set of biallelic SNPs from which all INDEL-adjacent sites are removed. 
+#The script devides tasks up along three sub-scripts. No user input is needed as soon as this script is initiated. 
 
 UNMERGED_VCF_DIR=$1 #The path to the directory in which all the unmerged VCF files are stored
 VCF_PREFIX=$2 #The name for the merged VCF. Prefix only, no need for ".vcf". 
 THREADS=$3 #The number of CPU cores to use
 
-# Merge the multiple small VCF files that were produced in the previous step
-# into one large VCF
+# Merge the multiple small VCF files that were produced in the previous step into one large VCF.
 bash /mnt/acorn_poolseq_pipeline/Concatenate_VCFs.sh ${UNMERGED_VCF_DIR} ${VCF_PREFIX} ${THREADS} 
 
 # Filter SNPs detected close to INDELs, as suggested by VarScan manual
