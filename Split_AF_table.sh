d27e5dfea76444635d2d865eea353634257c7064
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-06-20 18:59:28 +0200

    Create Split_AF_table.sh

---
+++ Split_AF_table.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Lars Littmann
+#20.06.2024
+#Take an allele frequency table and divide it up into 30 equally sized, numbered files.
+#This is done to parallelise lfmm.
+
+AF_TABLE=$1
+
+HEADER=$(head -n 1 ${AF_TABLE})
+
+tail -n +2 ${AF_TABLE} > body.txt
+
+split -n l/30 --numeric-suffixes=01 --additional-suffix .txt body.txt ${AF_TABLE/.txt/}
+
+sed -i "1i ${HEADER}" ${AF_TABLE/.txt/_Chunk??.txt}
