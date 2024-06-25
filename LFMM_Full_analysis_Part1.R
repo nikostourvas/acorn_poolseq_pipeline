4e55e1a939ffbd28f12219407b7018dc35e574da
LarsLittmann <larslittmann@gmail.com>
2024-06-25 15:18:04 +0000

    Lots of bug fixes to do with matrix sizes that now need to account for multiple env factors

--- LFMM_Full_analysis_Part1.R
+++ LFMM_Full_analysis_Part1.R
@@ -51,6 +51,7 @@ print(env.variables)
 
 env <- env.data[c(env.variables)]
 rownames(env) <- env.data$Plot_ID
+print("Check1")
 
 #### Import the genetic dataset ####
 
@@ -67,10 +68,14 @@ env.reduced <- env[rownames(env) %in% rownames(gen),]
 env <- env.reduced
 identical(as.character(rownames(env)),as.character(rownames(gen)))#check NAs
 
+print("Check2")
+
 gen.matrix <- as.matrix(gen)
 colnames(gen.matrix) <- NULL
 rownames(gen.matrix) <- NULL
 dim(gen.matrix)
+
+print("Check3")
 #write.table(gen.matrix, (paste(dir.path, "/res/", populations,"/", "genetic_data_", populations, "_Chunk_", chunk, ".lfmm", sep = "")), row.names = F, col.names = F, quote=F)
 
 ####import the thinned genetic data ####
@@ -85,6 +90,7 @@ colnames(gen.thin.matrix) <- NULL
 rownames(gen.thin.matrix) <- NULL
 #write.table(gen.thin.matrix, (paste("./res/", populations, "/", "gen_thinned_matrix", populations, "_Chunk_", chunk, ".lfmm", sep = "")), row.names = F, col.names = F, quote=F)
 
+print("Check4")
 #prepare
 X <- as.matrix(env) #for testing
 Y <- gen.matrix #The SNPs we are analysing in this implementation of the script
@@ -92,10 +98,9 @@ Z <- gen.thin.matrix #The thinned, genome-wide SNPs that we use to account for s
 Ks <- max.k #Kmax
 
 dir.create(paste(dir.path, "/res/", populations, sep=""), recursive=F)
-dir.create(paste(dir.path, "/res/", populations, "/environment_" , sep=""), recursive=F)
 for (i in 1:NCOL(X)) {
-  dir.create(paste(dir.path, "/res/", populations, "/environment_" env.variables[i], sep=""), recursive=F)
-  setwd(paste(dir.path, "/res/", populations, "/environment_" env.variables[i], sep=""))
+  dir.create(paste(dir.path, "/res/", populations, "/environment_", env.variables[i], sep=""), recursive=F)
+  setwd(paste(dir.path, "/res/", populations, "/environment_", env.variables[i], sep=""))
   for (j in 1:Ks) {
     dir.create(paste("K", j, "/", sep=""), recursive=F)
   }
@@ -103,38 +108,37 @@ for (i in 1:NCOL(X)) {
 fdr.thres <- c(0.05,0.01,0.001)
 fdr.output <- 0.05
 
-gif <- matrix(0, nrow=1, ncol=as.integer(Ks))
-rownames(gif) <- colnames(X)
-colnames(gif) <- paste("K", 1:Ks, sep="")
-
 nb.asso.q <- matrix(0,nrow=1, ncol=length(fdr.thres))
 rownames(nb.asso.q) <- "value"
 colnames(nb.asso.q) <- c("q0.05","q0.01","q0.001")
 
-nb.asso.k <- matrix(0, nrow=1, ncol=as.integer(Ks))
+print("Check5")
+
+nb.asso.k <- matrix(0, nrow=NCOL(X), ncol=as.integer(Ks))
 rownames(nb.asso.k) <- colnames(X)
 colnames(nb.asso.k) <- paste("K", 1:Ks, sep="")
 
+print("Check6")
 #### Fit an LFMM based on ridge estimates, i.e, compute B, U, V estimates ####
 for (j in 1:NCOL(X)) {
   for (i in 1:Ks) {
 
   print(paste("Generating Z-scores for environmental factor ", env.variables[j], "and K = ", i, sep=""))
 
-  setwd(paste(dir.path, "/res/", populations, "/environment_" env.variables[j], "/K", i, "/", sep=""))
+  setwd(paste(dir.path, "/res/", populations, "/environment_", env.variables[j], "/K", i, "/", sep=""))
   res <- matrix(nrow=NCOL(Y), ncol=2); rownames(res) <- colnames(Y); colnames(res) <- c("SNPid","zscore")
   mod.lfmm2 <- NULL; stats.lfmm2 <- NULL
     
   #Estimate latent factors and environmental effects using the regularised least-squares problem "ridge estimates"
 
-  mod.lfmm2 <- lfmm2(input=Z, env=X[,1], K=i, lambda=1e-5, effect.sizes=T)
+  mod.lfmm2 <- lfmm2(input=Z, env=X[,j], K=i, lambda=1e-5, effect.sizes=T)
     
   # Statistical tests on genotypic data with imputed missing dat
-  stats.lfmm2 <- lfmm2.test(object=mod.lfmm2, input=Y, env=X[,1], full=F, genomic.control=F) 
+  stats.lfmm2 <- lfmm2.test(object=mod.lfmm2, input=Y, env=X[,j], full=F, genomic.control=F) 
   res[,"SNPid"] <- snp.info #"SNPid" #res[,"SNPid"] <- snp.info$SNPid
   res[,"zscore"] <- stats.lfmm2$zscores
 
   # res[,"qvalue"] <- p.adjust(as.vector(stats.lfmm2$pvalues), method="fdr", n=length(stats.lfmm2$pvalues))
-  write.table(res, paste("LFMM_Zscores_", populations, "environment_", j, "_K", i, "_Chunk_", chunk, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F) # save all information per SNP
+  write.table(res, paste("LFMM_Zscores_", populations, "environment_", env.variables[j], "_K", i, "_Chunk_", chunk, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F) # save all information per SNP
   }
 }
