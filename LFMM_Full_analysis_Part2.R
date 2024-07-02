fe14493acd21ffeb5d026b1b631fea008c057f47
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-07-02 18:27:22 +0200

    Remove redundant script. Make it run for only one specified K. Add lots of documentation.

--- LFMM_Full_analysis_Part2.R
+++ LFMM_Full_analysis_Part2.R
@@ -1,3 +1,16 @@
+###############################################
+### Running LFMM for ACORN. Credit:         ###
+### Benjamin Dauphin and Christian Rellstab ###
+### Modified for paralel analysis by        ###
+### Lars Littmann                           ###
+###############################################
+
+####README####
+
+#This script can be used to process raw z-scores from lfmm to get final results.
+#The script merges z-scores from 50 chunks of the unthinned genomic dataset.
+#The script processes only one environmental variable. It should be run in parallel by environmental factor.
+
 #### Load packages ####
 #install.packages("BiocManager")
 #BiocManager::install("LEA")
@@ -13,12 +26,12 @@ rm(list = ls())
 args=scan(text=commandArgs(trailingOnly = TRUE),what="")
 dir.path <- args[1] #First string to receive is the output directory
 populations <- args[2] #Path to the thinned genomic dataset. Needs to be imputed first.
-max.k <- as.numeric(args[3]) #Maximum number of Ks.
+set.k <- as.numeric(args[3]) #The value of K that was set for this analysis. 
 env.variable<-args[4] #The environmental factor this scirpt operates on (parallel to other environmental factors)
 
-gif.matrix <- matrix(nrow = 1, ncol = max.k)
+gif.matrix <- matrix(nrow = 1, ncol = 1)
 rownames(gif.matrix) <- c("gif")
-colnames(gif.matrix) <- c(1:max.k)
+colnames(gif.matrix) <- c(1)
 
 fdr.thres <- c(0.05,0.01,0.001)
 fdr.output <- 0.05
@@ -27,39 +40,48 @@ nb.asso.q <- matrix(nrow=1, ncol=length(fdr.thres))
 rownames(nb.asso.q) <- "value"
 colnames(nb.asso.q) <- c("q0.05","q0.01","q0.001")
 
-nb.asso.k <- matrix(nrow=1, ncol=as.integer(max.k))
+nb.asso.k <- matrix(nrow=1, ncol=1)
 rownames(nb.asso.k) <- colnames(env.variable)
-colnames(nb.asso.k) <- paste("K", 1:max.k, sep="")
+colnames(nb.asso.k) <- paste("K1")
 
-for(i in 1:max.k) {
-  setwd(paste(dir.path, "/res/", populations, "/environment_", env.variable, "/K", i, "/",sep=""))
+###Merge the Z-scores of all 50 chunks of unthinned genetic data. This is the main output of Part 1. 
 
-  merged.zscores<-read.csv(paste("LFMM_Zscores_", populations, "environment_", env.variable, "_K", i, "_Chunk_1.csv", sep=""), header = T, sep = ",")
-  print(paste("K ", i,", Chunk 1", sep=""))
-  for(j in 2:50) {
+setwd(paste(dir.path, "/res/", populations, "/Full_analysis_K_", set.k, "/environment_", env.variable, "/", sep="")) #Set the working directory that contains all the chunk files.
+
+merged.zscores<-read.csv(paste("LFMM_Zscores_", populations, "environment_", env.variable, "_K", set.k, "_Chunk_1.csv", sep=""), header = T, sep = ",") #Start the data frame off with chunk 1.
+print(paste("Environmental factor ", env.variable, ", Chunk 1", sep="")) #Give the user some output to track the script's progress. Remains invisible in parallel applications.
+for(j in 2:50) {
+
+#This piece of code is defunct for now, but I would still like to keep this solution to the problem of leading zeros in the code, in case it's ever relevant.    
 #    if (j<10) {
-#      zscores.chunk<-read.csv(paste(dir.path, "/res/", populations, "/selectingK/K", i, "/LFMM_Zscores_", populations, "_K", i,"_Chunk_0", j, ".csv", sep="" ), header = T, sep = ",")
+#      zscores.chunk<-read.csv(paste(dir.path, "/res/", populations, "/Full_analysis_K_", set.k, "/environment_", env.variable, "/LFMM_Zscores_", populations, "_K", set.k,"_Chunk_0", j, ".csv", sep="" ), header = T, sep = ",")
 #      print(paste("K ", i,", Chunk ", j, sep=""))  
 #  } else {
-      zscores.chunk<-read.csv(paste("LFMM_Zscores_", populations, "environment_", env.variable, "_K", i,"_Chunk_", j, ".csv", sep=""), sep=",", header=T)
-      print(paste("K ", i,", Chunk ", j, sep=""))
-    #}
-    merged.zscores<-rbind(merged.zscores, zscores.chunk)
+    
+    zscores.chunk<-read.csv(paste("LFMM_Zscores_", populations, "environment_", env.variable, "_K", set.k,"_Chunk_", j, ".csv", sep=""), sep=",", header=T) #Load the z-scores of the chunk we are looping over into a df.
+    print(paste("Environmental factor ", env.variable, ", Chunk ", j, sep=""))  #Give the user some output to track the script's progress. Remains invisible in parallel applications.
+  
+  #} #Still part of the solution for leading zeros. This bracket is commented out and can be ignored. 
+  
+    merged.zscores<-rbind(merged.zscores, zscores.chunk) #Append the chunk we are looping over at the bottom of the df that we have compiled up to now.
+  
   }
-  print(paste("Calculating GIF for K ", i, sep=""))
+  print(paste("Calculating GIF" sep=""))
 
-  gif <- median((merged.zscores$zscore)^2)*(qchisq(0.5, df = 1, lower.tail = FALSE))
-  gif.matrix[1,i] <- gif
+  gif <- median((merged.zscores$zscore)^2)*(qchisq(0.5, df = 1, lower.tail = FALSE)) #Calculate the genomic inflation factor based on all the Z-scores.
+  gif.matrix[1,1] <- gif #Store the genomic inflation factor for later.
 
-  print(paste("Calculating P-values for K ", i, sep=""))
-  results.df <- as.data.frame(merged.zscores)
-  results.df$pvalues <- pchisq(results.df$zscore^2/gif, df = 1, lower.tail = FALSE)
-  qv.lfmm2 <- qvalue::qvalue(as.vector(results.df$pvalues), fdr.level=fdr.output)
-  results.df$qvalues <- qv.lfmm2$qvalues
-  #write.table(results.df, paste("LFMM_AllResults_env_", env.variable, "_K", i, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F) # save all information per SNP
+  print(paste("Calculating P-values" sep="")) #Give the user an indicator of the progress. Not visible during parallel processing. 
+  results.df <- as.data.frame(merged.zscores) #Rename the dataframe that we used to merge all the z-scores.
 
-  #Distribution of p-values 
-   png(paste(dir.path, "/res/", populations, "/environment_", env.variable, "/K", i, "/PvalueDistribution_", populations, "_environment_", env.variable, "_K", i, ".png", sep=""), units = "px", width=2500, height=1500)
+  results.df$pvalues <- pchisq(results.df$zscore^2/gif, df = 1, lower.tail = FALSE) #Calculate all the P-values based on z-scores corrected with the GIF.
+  qv.lfmm2 <- qvalue::qvalue(as.vector(results.df$pvalues), fdr.level=fdr.output) #Calculate q-values.
+  results.df$qvalues <- qv.lfmm2$qvalues #Add q-values to the dataframe with our results. 
+
+  #write.table(results.df, paste("LFMM_AllResults_env_", env.variable, "_K", set.k, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F) # save all information per SNP. Commented out to save disk space.
+
+  #Plot the distribution of p-values. We plot them as PNGs, because the PDFs contained so many points that it took 5 minutes to load each one.
+   png(paste(dir.path, "/res/", populations, "/Full_analysis_K_", set.k, "/environment_", env.variable, "/PvalueDistribution_", populations, "_environment_", env.variable, "_K", set.k, ".png", sep=""), units = "px", width=2500, height=1500)
     par(mfrow=c(1,2), mar=c(5, 5, 4, 1))
     hist(results.df$pvalues, col="red", main="P-value distribution")
     qqplot(rexp(length(results.df$pvalues), rate=log(10)), -log10(results.df$pvalues), xlab="Expected quantile", pch=19, cex=1)
@@ -73,37 +95,33 @@ for(i in 1:max.k) {
       v <- qv.lfmm2$qvalues
       w <- which(sort(v) <= q)
       
-      print(paste("Assessing candidates for K=", i, ", and fdr threshold ", q, sep=""))
+      print(paste("Assessing candidates for fdr threshold ", q, sep=""))
 
       candidate <- order(results.df$qvalues, decreasing=F)[w]
-      png(paste("ManhattanPlot_env", env.variable, "_K", i, "_q", q, ".png", sep=""), units="px", width=2500, height=1500)
+      png(paste("ManhattanPlot_env", env.variable, "_K", set.k, "_q", q, ".png", sep=""), units="px", width=2500, height=1500)
       par(mfrow=c(1,1), mar=c(5, 5, 4, 1))
-      manhattan <- plot(-log10(results.df$pvalues), main=paste("Manhattan plot | env", env.variable, " | K", i, " | q=", q, sep=""), cex.main=1.2, xlab="Locus", ylab="-Log(P-value)", cex=.7, col="grey")
+      manhattan <- plot(-log10(results.df$pvalues), main=paste("Manhattan plot | env", env.variable, " | K", set.k, " | q=", q, sep=""), cex.main=1.2, xlab="Locus", ylab="-Log(P-value)", cex=.7, col="grey")
       points(candidate, -log10(results.df$pvalues)[candidate], pch=19, cex=1, col="red")
       dev.off()
 
       if(q == fdr.output) {
-        nb.asso.k[1,i] <- length(candidate)
+        nb.asso.k[1,1] <- length(candidate)
       }
 
       nb.asso.q["value",x] <- length(candidate)
       candidate <- results.df[candidate,]
       
       if(NCOL(candidate) == 1) {
-        print("In the first possible scenario of the if else statement")
-        write.table(t(candidate), paste("CandidatesOrdered_env", env.variable, "_K", i, "_q", q, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F)
+        write.table(t(candidate), paste("CandidatesOrdered_env", env.variable, "_K", set.k, "_q", q, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F)
       } else {
         if(NCOL(candidate) > 1) {
-          print("In the second possible scenario of the if else statement")
-          write.table(candidate[order(as.numeric(candidate$pvalue), decreasing=F),], paste("CandidatesOrdered_env", env.variable, "_K", i, "_q", q, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F)
+          write.table(candidate[order(as.numeric(candidate$pvalue), decreasing=F),], paste("CandidatesOrdered_env", env.variable, "_K", set.k, "_q", q, ".csv", sep=""), sep=",", row.names=F, col.names=T, quote=F)
         } else {
-          print("In the third possile scenario of the if else statement")
-          write.table(candidate, paste("CandidatesOrdered_env", env.variable, "_K", i, "_q", q, ".csv", sep=""), sep=",", row.names=T, col.names=F, quote=F)
+          write.table(candidate, paste("CandidatesOrdered_env", env.variable, "_K", set.k, "_q", q, ".csv", sep=""), sep=",", row.names=T, col.names=F, quote=F)
         }
       }
     }
-    write.table(nb.asso.q, paste("AssociationsNb_env", env.variable, "_K", i, "_q.csv", sep=""), sep=",", row.names=F, col.names=T, quote=F)
-}
+    write.table(nb.asso.q, paste("AssociationsNb_env", env.variable, "_K", set.k, "_q.csv", sep=""), sep=",", row.names=F, col.names=T, quote=F)
 
-write.table(gif.matrix, file = paste(dir.path, "/res/", populations, "/environment_", env.variable, "/", populations, "_GIFs.txt", sep = ""), sep = ",", quote = F, row.names = F, col.names = T)
+write.table(gif.matrix, file = paste("GIF_", env.variable, "_K_" set.k, ".txt", sep = ""), sep = ",", quote = F, row.names = F, col.names = T)
 
