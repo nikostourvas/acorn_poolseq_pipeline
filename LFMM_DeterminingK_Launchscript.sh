49d7b76c8db9c5be2f84d0ea610a3e7cef80b6e8
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-07-03 12:57:44 +0200

    Lots of documentation and added an output file that helps launch the second half of the analysis.

--- LFMM_DeterminingK_Launchscript.sh
+++ LFMM_DeterminingK_Launchscript.sh
@@ -17,12 +17,14 @@
 #Missingness per SNP should be kept below 10%, otherwise this imputation method is not reliable.
 
 # declare variables
-INPUT_AF_TABLE=$1
-INPUT_THINNED_AF_TABLE=${INPUT_AF_TABLE/AlleleFrequencyTable.txt/Thinned_500bp_AlleleFrequencyTable.txt}
+INPUT_AF_TABLE=$1 #The complete allele frequency table as produced by the script Make_Frequency_Table.sh. No need to perform any imputation etc. beforehand. Needs to be placed in ./dat
+INPUT_THINNED_AF_TABLE=${INPUT_AF_TABLE/AlleleFrequencyTable.txt/Thinned_500bp_AlleleFrequencyTable.txt} #The thinned allele frequency table as produced by Make_Thinned_AFtable.sh. 
+                                                                                                         #No need to perform any imputation etc. beforehand. Needs to be placed in ./dat
 
-ENV_DATA=$2
-MAX_K=$3
-POPULATIONS=$4
+ENV_DATA=$2 #A csv that contains all the environmental data (in columns) for each of the populations (in rows). Population names need to correspond with the genetic data.
+MAX_K=$3 #The maximum value of K that you wish the script to analyse. The script will only run for multiple instance of K (1-maxK)
+POPULATIONS=$4 #The 3-character code that specifies which (sub)set of ACORN populations is used.
+ENV_VARIABLES=$5 #A list seperated by commas WITHOUT SPACES of the names of all the environmental factors you wish to include in the analysis.
 
 #Create a good filename for the output.
 IMPUTED_TABLE=$(basename ${INPUT_AF_TABLE/.txt/_Imputed_by_Mean.txt})
@@ -35,6 +37,8 @@ mkdir -p ${OUTPUT_DIR}
 #The first line creates a new column that contains the mean of all values. 
 #The second line replaces 'NA' values with the mean which is contained in the last column.
 #At the end of the second line, the column containing the mean values is removed. 
+#The fourth line removes any lines in which there is no variance. Required for LFMM to work.
+#This takes care of an edge case where only one population has a valid allele frequency and the rest of the populations registers as NA
 awk -F "\t" ' OFS="\t" {sum = 0; j = 1; MEANpos = NF+1; for (i = 2; i <= NF; i++) if ($i=='NA') {j++} else (sum+=$i); sum /= (NF-j); $MEANpos=sum; print $0 }' ${INPUT_AF_TABLE} | \
 awk -F "\t" ' OFS="\t" {sum = 0; MEANpos=NF; VARpos=NF+1; for (i=2; i<=NF-1; i++) sum+=($1-$MEANpos)^2; $VARpos=sum; print $0 }' | \
 awk -F "\t" ' OFS="\t" {MEANpos= NF-1; VARpos=NF; for (i=2; i <= NF; i++) if ($i=="NA") {$i=$MEANpos}; if ($VARpos!=0) {print $0}} ' | \
@@ -48,7 +52,7 @@ awk -F "\t" ' OFS="\t" {NF-=2}1' > ${OUTPUT_DIR}/${IMPUTED_THINNED_TABLE}
 
 ###SECOND STEP###
 #The splitter-upper
-#Splits up the large, unthinned AFtable into 50 roughly equal-size tables.
+#Splits up the large, unthinned AFtable into 50 roughly equal-size tables. The thinned dataset is small enough as it is and should not be split.
 
 HEADER=$(head -n 1 ${OUTPUT_DIR}/${IMPUTED_TABLE}) #Store the head line of the table in a string for later.
 tail -n +2 ${OUTPUT_DIR}/${IMPUTED_TABLE} > body.txt #Create a file that contains the whole table EXCEPT for the header line.
@@ -60,14 +64,26 @@ rm body.txt #Remove the header-less table that we created.
 #The R initiation-station
 #Start by creating a file that contains all the parameters for each job. Then use this file to instruct GNU parallel.
 
-realpath ${OUTPUT_DIR}/${IMPUTED_TABLE/.txt/_Chunk??.txt} > LFMM_DeterminingK_Parameters_${POPULATIONS}_INTERMEDIATE.txt
+realpath ${OUTPUT_DIR}/${IMPUTED_TABLE/.txt/_Chunk??.txt} > LFMM_DeterminingK_Parameters_${POPULATIONS}_INTERMEDIATE.txt #Use the path of all 50 split genetic datasets as a scaffold to create the parameters file.
 
+#The following awk command adds all the other parameters in the order that the next R-script expects. Separated by a space (except for the environmental variables, which are given as a comma-seperated list).
 awk -F " " -v awk_working_directory="${PWD}" -v awk_thinned_dataset="${OUTPUT_DIR}/${IMPUTED_THINNED_TABLE}" \
--v awk_environmental_data="${ENV_DATA}" -v awk_max_k="${MAX_K}" -v awk_populations="${POPULATIONS}" \
-' OFS=" " {print awk_working_directory, $0, awk_thinned_dataset, awk_environmental_data, awk_max_k, awk_populations, NR}' \
+-v awk_environmental_data="${ENV_DATA}" -v awk_max_k="${MAX_K}" -v awk_populations="${POPULATIONS}" -v awk_environmental_factors=${ENV_VARIABLES} \
+' OFS=" " {print awk_working_directory, $0, awk_thinned_dataset, awk_environmental_data, awk_max_k, awk_populations, NR, awk_environmental_factors}' \
 LFMM_DeterminingK_Parameters_${POPULATIONS}_INTERMEDIATE.txt > LFMM_DeterminingK_Parameters_${POPULATIONS}.txt
 
 rm LFMM_DeterminingK_Parameters_${POPULATIONS}_INTERMEDIATE.txt #Get rid of unneeded intermediate file.
+
+###BONUS STEP###
+#Create a file that can be used to launch the next Rscript as well. The user can use this file to launch the second R-script with a gnu parallel command.
+
+echo ${ENV_VARIABLES} | tr -s ',' '\n' > LFMM_Full_Analysis_Part2_${POPULATIONS}_INTERMEDIATE.txt #This time, use the list of environmental variables as a scaffold for the file. The comma-separated variable names are properly seperated. 
+
+#The following awk command adds all the other parameters in the order that the next R-script expects. Separated by a space.
+awk -F " " -v awk_working_directory="${PWD}" -v awk_populations="${POPULATIONS}" -v awk_set_k="${SET_K}" \
+'OFS=" " {print awk_working_directory, awk_populations, awk_set_k, $0}' LFMM_Full_Analysis_Part2_${POPULATIONS}_INTERMEDIATE.txt > LFMM_Full_Analysis_Part2_Parameters_${POPULATIONS}.txt
+
+rm LFMM_Full_Analysis_Part2_${POPULATIONS}_INTERMEDIATE.txt
 
 ###THE GRANDE FINALE###
 #Actually launching the lfmm jobs
