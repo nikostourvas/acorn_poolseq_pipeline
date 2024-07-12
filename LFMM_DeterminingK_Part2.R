23f08271185e486eba496b9890e941b8d0407152
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-07-12 11:22:58 +0200

    Instead of setting a wd, just give full paths with variables for brevity

--- LFMM_DeterminingK_Part2.R
+++ LFMM_DeterminingK_Part2.R
@@ -20,17 +20,17 @@ gif.matrix <- matrix(nrow = 1, ncol = max.k)
 rownames(gif.matrix) <- c("gif")
 colnames(gif.matrix) <- c(1:max.k)
 
-setwd(paste(dir.path, "/res/", populations, "/selectingK/EnvironmentalFactor_", env.variable, "/", sep=""))
+subdir.path<-(paste(dir.path, "/res/", populations, "/selectingK/EnvironmentalFactor_", env.variable, "/", sep=""))
 
 for(i in 1:max.k) {
-  merged.zscores<-read.csv(paste(dir.path, "/res/", populations, "/selectingK/EnvironmentalFactor_", env.variable, "/K", i, "/LFMM_Zscores_", populations, "_EnvironmentalFactor_", env.variable, "_K", i, "_Chunk_1.csv", sep=""), header = T, sep = ",")
+  merged.zscores<-read.csv(paste(subdir.path, "K", i, "/LFMM_Zscores_", populations, "_EnvironmentalFactor_", env.variable, "_K", i, "_Chunk_1.csv", sep=""), header = T, sep = ",")
   print(paste("K ", i,", Chunk 1", sep=""))
   for(j in 2:50) {
 #    if (j<10) {
 #      zscores.chunk<-read.csv(paste(dir.path, "/res/", populations, "/selectingK/K", i, "/LFMM_Zscores_", populations, "_K", i,"_Chunk_0", j, ".csv", sep="" ), header = T, sep = ",")
 #      print(paste("K ", i,", Chunk ", j, sep=""))  
 #  } else {
-      zscores.chunk<-read.csv(paste(dir.path, "/res/", populations, "/selectingK/EnvironmentalFactor_", env.variable, "/K", i, "/LFMM_Zscores_", populations, "_EnvironmentalFactor_", env.variable, "_K", i, "_Chunk_", j, ".csv", sep=""), sep=",", header=T)
+      zscores.chunk<-read.csv(paste(subdir.path, "K", i, "/LFMM_Zscores_", populations, "_EnvironmentalFactor_", env.variable, "_K", i, "_Chunk_", j, ".csv", sep=""), sep=",", header=T)
       print(paste("K ", i,", Chunk ", j, sep=""))
     #}
     merged.zscores<-rbind(merged.zscores, zscores.chunk)
@@ -45,7 +45,7 @@ for(i in 1:max.k) {
   results.df$pvalues <- pchisq(results.df$zscore^2/gif, df = 1, lower.tail = FALSE)
   
   #Distribution of p-values 
-   png(paste(dir.path, "/res/", populations, "/selectingK/EnvironmentalFactor_", env.variable, "/PvalueDistribution_", populations, "EnvironmentalFactor_", env.variable, "_K", i, ".png", sep=""), units = "px", width=2500, height=1500)
+   png(paste(subdir.path, "PvalueDistribution_", populations, "EnvironmentalFactor_", env.variable, "_K", i, ".png", sep=""), units = "px", width=2500, height=1500)
     par(mfrow=c(1,2), mar=c(5, 5, 4, 1))
     hist(results.df$pvalues, col="red", main="P-value distribution")
     qqplot(rexp(length(results.df$pvalues), rate=log(10)), -log10(results.df$pvalues), xlab="Expected quantile", pch=19, cex=.4)
@@ -53,5 +53,5 @@ for(i in 1:max.k) {
     dev.off()
   
 }
-write.table(gif.matrix, file = paste(dir.path, "/res/", populations, "/selectingK/EnvironmentalFactor_", env.variable, "/EnvironmentalFactor_", env.variable, "_GIFs.txt", sep = ""), sep = ",", quote = F, row.names = F, col.names = T)
+write.table(gif.matrix, file = paste(subdir.path, "/EnvironmentalFactor_", env.variable, "_GIFs.txt", sep = ""), sep = ",", quote = F, row.names = F, col.names = T)
 
