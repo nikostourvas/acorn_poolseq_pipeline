8365a4e26468b2f7c3720cccf0fd12c5b31fb2fe
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-07-18 12:04:55 +0200

    Make sure the full genetic data is passed on to the part2 R-script for plotting.

--- LFMM_Full_analysis_Launchscript.sh
+++ LFMM_Full_analysis_Launchscript.sh
@@ -79,8 +79,8 @@ rm LFMM_Full_Analysis_${POPULATIONS}_INTERMEDIATE.txt #Get rid of unneeded inter
 echo ${ENV_VARIABLES} | tr -s ',' '\n' > LFMM_Full_Analysis_Part2_${POPULATIONS}_INTERMEDIATE.txt #This time, use the list of environmental variables as a scaffold for the file. The comma-separated variable names are properly seperated. 
 
 #The following awk command adds all the other parameters in the order that the next R-script expects. Separated by a space.
-awk -F " " -v awk_working_directory="${PWD}" -v awk_populations="${POPULATIONS}" -v awk_set_k="${SET_K}" \
-'OFS=" " {print awk_working_directory, awk_populations, awk_set_k, $0}' LFMM_Full_Analysis_Part2_${POPULATIONS}_INTERMEDIATE.txt > ./par/LFMM_Full_Analysis_Part2_Parameters_${POPULATIONS}.txt
+awk -F " " -v awk_working_directory="${PWD}" -v awk_populations="${POPULATIONS}" -v awk_set_k="${SET_K}" -v awk_genetic_dataset="${OUTPUT_DIR}/${IMPUTED_TABLE}" \
+'OFS=" " {print awk_working_directory, awk_populations, awk_set_k, $0, awk_genetic_dataset}' LFMM_Full_Analysis_Part2_${POPULATIONS}_INTERMEDIATE.txt > ./par/LFMM_Full_Analysis_Part2_Parameters_${POPULATIONS}.txt
 
 rm LFMM_Full_Analysis_Part2_${POPULATIONS}_INTERMEDIATE.txt
 
