4d5ee35504cfe1d49be7ee5a27a4b90d1b7c8ee7
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-07-18 11:38:19 +0200

    Add lines for plotting results

--- LFMM_Full_analysis_Part2.R
+++ LFMM_Full_analysis_Part2.R
@@ -144,3 +144,32 @@ colnames(gen.matrix) <- NULL
 rownames(gen.matrix) <- NULL
 dim(gen.matrix)
 
+#### Plot SNPs significantly associated to environmental conditions ####
+fdr.output <- 0.05
+setwd(paste(dir.path,"lfmm/",codesp[sp],sep=""))
+for (j in 1:NCOL(X)) {
+  for (i in 1:Ks) { 
+    candidate.list <- read.table(paste("env", j, "/K", i, "/CandidatesOrdered_env", j, "_K", i, "_q", fdr.output, ".csv", sep=""), row.names="SNPid", header=T, sep=",")
+    tmp.gen <- gen
+    colnames(tmp.gen) <- snp.info # "SNPid" # snp.info$SNPid
+    tmp.gen.t <- t(tmp.gen); rownames(tmp.gen.t) <- colnames(tmp.gen); colnames(tmp.gen.t) <- rownames(tmp.gen)
+    candidate.gen <- merge(candidate.list, tmp.gen.t, by="row.names"); colnames(candidate.gen)[1] <- "SNPid"
+    candidate.gen.ordered <- candidate.gen[order(candidate.gen$pvalue, decreasing=F),]
+    if (NROW(candidate.list) > 0) {
+      genotypes <- t(candidate.gen.ordered[,5:NCOL(candidate.gen.ordered)])
+      pdf(paste("env", j, "/K", i, "/PlotOfSignifCandidates_K", i, "_q", fdr.output, ".pdf", sep=""), width=10, height=10)
+      par(mfrow=c(2,2), mar=c(5, 5, 1, 1))
+      for (p in 1:NROW(candidate.list)) {
+        plot(X[,j], genotypes[,p], pch=20, cex=1.5, xlab=colnames(X)[j], ylab="Genotype frequency [-]",
+             cex.lab=1.2, ylim=c(0, 1), cex.main=1.2, col=alpha("blue",0.2))
+        zs <- candidate.list$zscore[p]
+        pv <- candidate.list$pvalue[p]
+        qv <- candidate.list$qvalue[p]
+        legend(min(X[,j]), 1.5, legend=c(paste(rownames(candidate.list)[p]), paste("z-score =", format(zs, digits=3)),
+                                           paste("p-value =", format(pv, digits=2)), paste("q-value =", format(qv, digits=4))), bty='n', cex=0.7)
+      }
+      dev.off()
+    }
+  }
+}
+
