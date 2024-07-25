75727fa2450f2640f74fb1a9b639e329e4c55bd1
Lars <138503061+LarsLittmann@users.noreply.github.com>
2024-07-25 14:05:01 +0200

    Make sure the median corrected p-value is shown in the relevant qqplot

--- LFMM_DeterminingK_Part2.R
+++ LFMM_DeterminingK_Part2.R
@@ -79,7 +79,7 @@ for(i in 1:max.k) { #Loop for the number of Ks we wish to evaluate.
     hist(results.df$corrected_pvalues, col="red", main="Corrected P-value distribution")
     qqplot(rexp(length(results.df$corrected_pvalues), rate=log(10)), -log10(results.df$corrected_pvalues), xlab="Expected quantile", pch=19, cex=2)
     abline(coef=c(0,1))
-    legend("bottomright", title=c(paste("MedianP = ", median(-log10(results.df$pvalue)), sep=""))) #Display the median pvalue (-log10 transformed) 
+    legend("bottomright", title=c(paste("MedianP = ", median(-log10(results.df$corrected_pvalues)), sep=""))) #Display the median pvalue (-log10 transformed) 
     dev.off()
   
 }
